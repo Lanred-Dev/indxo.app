340684b09d5f93786991569fb5281ba91ceb0366
Lanred-Dev <76853738+Lanred-Dev@users.noreply.github.com>
2025-01-13 23:34:02 -0600

    add PageSection and SimplePage components

---
+++ src/lib/components/SimplePage.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    import PageSection, { type props } from "./PageSection.svelte";
+
+    let sectionProps: props = $props();
+</script>
+
+<PageSection
+    {...sectionProps}
+    classes="min-h-screen"
+    style={(sectionProps.style ?? "") + "padding-top: 25vh; justify-content: flex-start;"}
+/>
