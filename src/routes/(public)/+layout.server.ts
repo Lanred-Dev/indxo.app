f5ef86d7a5be0efea3156a51b1ce45665e6286d5
Lanred-Dev <76853738+Lanred-Dev@users.noreply.github.com>
2025-01-13 23:50:30 -0600

    fix: invert session check in load function to redirect authenticated users

--- src/routes/(public)/+layout.server.ts
+++ src/routes/(public)/+layout.server.ts
@@ -3,7 +3,7 @@ import { redirect } from "@sveltejs/kit";
 export async function load({ locals: { auth } }) {
     const session = await auth();
 
-    if (!session) {
+    if (session) {
         throw redirect(307, "/");
     }
 }
