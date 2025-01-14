55db4c6f142024330867a7a2f8eda1a2f535e3cb
Lanred-Dev <76853738+Lanred-Dev@users.noreply.github.com>
2025-01-13 23:45:16 -0600

    add authentication redirection for login and registration pages

---
+++ src/routes/(public)/login/+page.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+    import SimplePage from "$lib/components/SimplePage.svelte";
+    import { signIn } from "@auth/sveltekit/client";
+</script>
+
+{#snippet provider(text: string, icon: string, providerID: string)}
+    <li>
+        <button class="primary-dark w-full" onclick={() => signIn(providerID)}>
+            <img class={providerID === "github" ? "dark:invert" : ""} src={icon} alt={text} />
+            {text}
+        </button>
+    </li>
+{/snippet}
+
+<SimplePage
+    title="Create your portfolio today."
+    text="Sign in with your preferred provider and get started."
+>
+    <ul class="space-y-3">
+        {@render provider("Sign in with Google", "/icons/Google.svg", "google")}
+    </ul>
+</SimplePage>
