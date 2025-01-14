dc297838321cc13d629f3f48c4893a1a551be5a7
Lanred-Dev <76853738+Lanred-Dev@users.noreply.github.com>
2025-01-13 23:45:24 -0600

    add server-side authentication check and redirect to login

---
+++ src/routes/(protected)/+layout.server.ts
@@ -0,0 +1,9 @@
+import { redirect } from "@sveltejs/kit";
+
+export async function load({ locals: { auth } }) {
+    const session = await auth();
+
+    if (!session) {
+        throw redirect(307, "/login");
+    }
+}
