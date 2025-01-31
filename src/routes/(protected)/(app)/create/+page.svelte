6b8b4f8a0b7fe51468b1c03ccbbc5a1208dd119b
Landon Redmond <landon.redmond0@gmail.com>
2025-01-30 20:55:03 -0600

    feat: add Folder creation form with icon selection and description

---
+++ src/routes/(protected)/(app)/create/+page.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    import FolderForm from "./Forms/Folder.svelte";
+    import SetForm from "./Forms/Set.svelte";
+    import { onMount } from "svelte";
+
+    let type: "folder" | "set";
+
+    onMount(() => {
+        const params = new URLSearchParams(location.search);
+        type = (params.get("type") as any) || "folder";
+    });
+</script>
+
+<div class="x-center y-center w-2/3">
+    {#if type === "folder"}
+        <FolderForm classes="w-full" action="/create" />
+    {:else}
+        <SetForm />
+    {/if}
+</div>
---
+++ src/routes/(protected)/(app)/create/Forms/Folder.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+    import { Form, FormRow, FormInput, FormSubmit, type props } from "$lib/components/Form";
+
+    let formProps: props = $props();
+</script>
+
+<Form {...formProps}>
+    <FormRow>
+        <FormInput
+            id="icon"
+            label="Icon"
+            type="dropdown"
+            options={[
+                { value: "Folder", image: "/icons/folder/Folder.svg" },
+                { value: "Camera", image: "/icons/folder/Camera.svg" },
+                { value: "Briefcase", image: "/icons/folder/Briefcase.svg" },
+            ]}
+            placeholder="John Doe"
+        />
+        <FormInput id="name" label="Name" type="text" placeholder="Most awesome name" />
+    </FormRow>
+
+    <FormInput
+        id="about"
+        label="What is this folder for?"
+        type="textarea"
+        placeholder="Something important..."
+        classes="h-40"
+    />
+
+    <FormSubmit text="Send" />
+</Form>
