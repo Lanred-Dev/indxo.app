3e1e9f44cafd7d78cd41039fb5ee0ff5b3a40553
Landon Redmond <landon.redmond0@gmail.com>
2025-01-30 22:02:11 -0600

    feat: implement form submission handling and enhance input type definitions

--- src/lib/components/Form/Form.svelte
+++ src/lib/components/Form/Form.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
-    import { onMount, type Snippet } from "svelte";
+    import { type Snippet } from "svelte";
     import { twMerge } from "tailwind-merge";
+    import { type inputType } from "./FormInput.svelte";
 
     export type props = {
         action: string;
@@ -10,8 +11,40 @@
     };
 
     let { action, method = "POST", classes, children }: props = $props();
+
+    function onSubmit(event: Event) {
+        event.preventDefault();
+
+        const form: HTMLFormElement = event.target as HTMLFormElement;
+        const formInputs: NodeListOf<HTMLElement> = form.querySelectorAll(".formInput")!;
+        const data: { [key: string]: any } = {};
+
+        formInputs.forEach((input: HTMLElement) => {
+            const inputType: inputType = input.getAttribute("data-type") as inputType;
+            let id: string = input.getAttribute("data-id")!;
+            let value: string | number = "";
+
+            if (inputType === "dropdown") {
+                const dropdown: HTMLElement = input.querySelector(".dropdown")!;
+                value = dropdown.getAttribute("data-value")!;
+            } else if (inputType === "textarea") {
+                const textarea: HTMLTextAreaElement = input.querySelector("textarea")!;
+                value = textarea.value;
+            } else {
+                const inputElement: HTMLInputElement = input.querySelector("input")!;
+                value = inputElement.value;
+            }
+
+            data[id] = value;
+        });
+
+        fetch(action, {
+            method,
+            body: JSON.stringify(data),
+        });
+    }
 </script>
 
-<form class={twMerge("space-y-5", classes)} {method} {action}>
+<form class={twMerge("space-y-5", classes)} {method} {action} onsubmit={onSubmit}>
     {@render children?.()}
 </form>
--- src/lib/components/Form/FormInput.svelte
+++ src/lib/components/Form/FormInput.svelte
@@ -1,6 +1,19 @@
 <script lang="ts">
     import { Dropdown, DropdownItem } from "$lib/components/Dropdown";
     import { twMerge } from "tailwind-merge";
+    import type { props as DropdownItemProps } from "../Dropdown/DropdownItem.svelte";
+
+    export type inputType =
+        | "text"
+        | "file"
+        | "email"
+        | "radio"
+        | "checkbox"
+        | "dropdown"
+        | "number"
+        | "checkbox"
+        | "textarea"
+        | "password";
 
     let {
         id,
@@ -13,30 +26,26 @@
     }: {
         id: string;
         label?: string;
-        type:
-            | "text"
-            | "file"
-            | "email"
-            | "radio"
-            | "checkbox"
-            | "dropdown"
-            | "number"
-            | "checkbox"
-            | "textarea"
-            | "password";
-        placeholder?: string;
+        type: inputType;
+        placeholder?: string | DropdownItemProps;
         value?: string | number;
         classes?: string;
         options?: Array<string> | Array<{ value: string; text?: string; image?: string }>;
     } = $props();
 
+    const stringPlaceholder: string | undefined =
+        typeof placeholder === "string" ? placeholder : undefined;
     const inputClasses: string = twMerge(
         `${type !== "dropdown" ? "primary" : ""} w-full text-lg`,
         classes
     );
 </script>
 
-<div class="{type === 'dropdown' ? 'w-fit' : 'w-full'} space-y-1">
+<div
+    class="formInput space-y-1 {type === 'dropdown' ? 'w-fit' : 'w-full'}"
+    data-type={type}
+    data-id={id}
+>
     {#if label}
         <label class="text-light cursor-text select-none pl-3" for={id}>{label}</label>
     {/if}
@@ -52,9 +61,14 @@
             {/each}
         </Dropdown>
     {:else if type === "textarea"}
-        <textarea class="resize-none {inputClasses}" name={id} {id} {placeholder} {value}
+        <textarea
+            class="resize-none {inputClasses}"
+            name={id}
+            {id}
+            placeholder={stringPlaceholder}
+            {value}
         ></textarea>
     {:else}
-        <input class={inputClasses} name={id} {id} {type} {placeholder} {value} />
+        <input class={inputClasses} name={id} {id} {type} placeholder={stringPlaceholder} {value} />
     {/if}
 </div>
