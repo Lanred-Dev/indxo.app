aeb5c124ff29eb8df0eca799ecd2747a5bd9280f
Lanred-Dev <76853738+Lanred-Dev@users.noreply.github.com>
2025-02-01 09:23:22 -0600

    fix: update to use correct api url

--- src/routes/(protected)/(app)/set/[id]/+page.ts
+++ src/routes/(protected)/(app)/set/[id]/+page.ts
@@ -1,5 +1,5 @@
 export async function load({ fetch, params }) {
-    const set = await (await fetch(`/api/sets/${params.id}`)).json();
+    const set = await (await fetch(`/api/documents/set/${params.id}`)).json();
 
     return {
         ...set,
