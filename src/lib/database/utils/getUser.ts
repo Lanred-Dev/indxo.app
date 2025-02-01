2679066b1464f3d71b8192285a7db3f5348f657f
Lanred-Dev <76853738+Lanred-Dev@users.noreply.github.com>
2025-02-01 09:22:26 -0600

    docs: add note to getUser function documentation about idToDocument usage

--- src/lib/database/utils/getUser.ts
+++ src/lib/database/utils/getUser.ts
@@ -7,6 +7,8 @@ const users: Collection<User> = loadCollection("accounts", "users");
 /**
  * Retrieves a user from the database using their email.
  *
+ * NOTE: For most use cases `idToDocument` should be used instead this is only for when you need to find a user by email.
+ *
  * @param email The user email.
  * @returns The user if found, otherwise null.
  */
