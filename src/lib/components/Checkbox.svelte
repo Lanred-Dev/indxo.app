88e9782b27d242cb01d6726599018c9840cca88c
Landon Redmond <landon.redmond0@gmail.com>
2025-01-31 23:37:07 -0600

    feat: add Checkbox component with toggle functionality

---
+++ src/lib/components/Checkbox.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+    import CheckIcon from "$lib/icons/Check.svelte";
+    import XIcon from "$lib/icons/X.svelte";
+    import { twMerge } from "tailwind-merge";
+
+    let {
+        placeholder,
+        classes,
+        id,
+    }: {
+        placeholder?: boolean;
+        classes?: string;
+        id?: string;
+    } = $props();
+
+    let currentValue: boolean = $state(placeholder || false);
+</script>
+
+<div class={twMerge("checkbox relative", classes)} {id} data-value={currentValue}>
+    <button onclick={() => (currentValue = !currentValue)} data-input type="button">
+        {#if currentValue}
+            <CheckIcon classes="aspect-1 h-7" />
+        {:else}
+            <XIcon classes="aspect-1 h-7" />
+        {/if}
+    </button>
+</div>
