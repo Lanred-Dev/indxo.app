183db92bf2dd374424d6e8216a05f146831f277e
Landon Redmond <landon.redmond0@gmail.com>
2025-02-07 11:30:51 -0600

    feat: add isPublic check for set document

--- src/routes/(protected)/api/documents/set/[id]/+server.ts
+++ src/routes/(protected)/api/documents/set/[id]/+server.ts
@@ -5,6 +5,12 @@ import type { Set } from "$lib/database/documents/Set";
 export async function GET({ params }) {
     const set: Set = await idToDocument("sets", params.id);
 
+    if (!set.isPublic) {
+        return json({
+            error: "Could not find set.",
+        });
+    }
+
     return json({
         ...set,
     });
