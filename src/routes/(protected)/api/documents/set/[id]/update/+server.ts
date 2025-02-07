04ea14e96e06c7af16f085d9a30e4e315512456e
Landon Redmond <landon.redmond0@gmail.com>
2025-02-06 22:49:32 -0600

    feat: implement validation for updatable fields in document update endpoint

--- src/routes/(protected)/api/documents/set/[id]/update/+server.ts
+++ src/routes/(protected)/api/documents/set/[id]/update/+server.ts
@@ -1,14 +1,31 @@
 import { json } from "@sveltejs/kit";
 import { loadCollection } from "$lib/database/mongo";
 import { ObjectId, type Collection } from "mongodb";
-import type { Set } from "$lib/database/documents/Set";
+import { updatableFields, type Set } from "$lib/database/documents/Set";
 import type { User } from "$lib/database/documents/User";
 
 const users: Collection<User> = loadCollection("accounts", "users");
 const sets: Collection<Set> = loadCollection("documents", "sets");
 
-export async function POST({ params, locals }) {
+export async function POST({ params, request }) {
+    const newFields: { [key: string]: any } = await request.json();
+    const validFields: { [key: string]: any } = {};
     const id: ObjectId = new ObjectId(params.id);
+
+    for (const key in newFields) {
+        if (!(key in updatableFields) || typeof newFields[key] !== updatableFields[key]) continue;
+
+        validFields[key] = newFields[key];
+    }
+
+    await sets.updateOne(
+        {
+            _id: id,
+        },
+        {
+            $set: validFields,
+        }
+    );
 
     return json({
         success: true,
