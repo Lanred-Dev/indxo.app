124f3f77dd9162587b4fcd36392886e9f632c795
Landon Redmond <landon.redmond0@gmail.com>
2025-02-07 12:48:18 -0600

    feat: add permission check for document update in API route

--- src/routes/(protected)/api/documents/set/[id]/update/+server.ts
+++ src/routes/(protected)/api/documents/set/[id]/update/+server.ts
@@ -2,10 +2,18 @@ import { json } from "@sveltejs/kit";
 import { loadCollection } from "$lib/database/mongo";
 import { ObjectId, type Collection } from "mongodb";
 import { updatableFields, type Set } from "$lib/database/documents/Set";
+import permissionCheck from "$lib/utils/permissionCheck.js";
 
 const sets: Collection<Set> = loadCollection("documents", "sets");
 
-export async function POST({ params, request }) {
+export async function POST({ params, request, fetch, locals }) {
+    const response = await fetch(`/api/documents/set/${params.id}`);
+    const hasPermission = await permissionCheck(locals.userID, response);
+
+    if (hasPermission) {
+        return hasPermission;
+    }
+
     const newFields: { [key: string]: any } = await request.json();
     const validFields: { [key: string]: any } = {};
     const id: ObjectId = new ObjectId(params.id);
