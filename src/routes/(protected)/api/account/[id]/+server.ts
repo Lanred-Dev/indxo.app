69f9f8a5c13aaad87c0e9bf56bb0b640fd30ce77
Lanred-Dev <76853738+Lanred-Dev@users.noreply.github.com>
2025-02-09 23:11:53 -0600

    refactor: enhance account API to fetch sets and folders

--- src/routes/(protected)/api/account/[id]/+server.ts
+++ src/routes/(protected)/api/account/[id]/+server.ts
@@ -1,14 +1,18 @@
 import { json } from "@sveltejs/kit";
 import idToDocument from "$lib/utils/idToDocument";
 import type { User } from "$lib/database/documents/User";
+import type { Set } from "$lib/database/documents/Set";
+import type { Folder } from "$lib/database/documents/Folder";
 
-export async function GET({ params }) {
+export async function GET({ params, fetch }) {
     const user: User = await idToDocument("users", params.id);
+    const sets: Set[] = await (await fetch(`/api/account/${params.id}/sets`)).json();
+    const folders: Folder[] = await (await fetch(`/api/account/${params.id}/folders`)).json();
 
     return json({
         name: user?.name,
         image: user?.image,
-        sets: user?.sets,
-        folders: user?.folders,
+        sets: sets,
+        folders: folders,
     });
 }
