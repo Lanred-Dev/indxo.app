e0b45d116d84a8318754c968ca46054fc51c6987
Lanred-Dev <76853738+Lanred-Dev@users.noreply.github.com>
2025-02-09 23:27:21 -0600

    feat: implement GET endpoints for user folders and sets retrieval

---
+++ src/routes/(protected)/api/account/[id]/sets/+server.ts
@@ -0,0 +1,21 @@
+import { json } from "@sveltejs/kit";
+import idToDocument from "$lib/utils/idToDocument";
+import type { Set } from "$lib/database/documents/Set";
+import type { User } from "$lib/database/documents/User";
+
+export async function GET({ params }) {
+    const user: User = await idToDocument("users", params.id);
+    const sets: Set[] = [];
+
+    for (const id of user.sets) {
+        const set: Set = await idToDocument("sets", id);
+
+        if (!set) {
+            continue;
+        }
+
+        sets.push(set);
+    }
+
+    return json(sets);
+}
