7f8327d014cb9de880f082bac0f033d809f7f814
Lanred-Dev <76853738+Lanred-Dev@users.noreply.github.com>
2025-02-09 23:14:00 -0600

    refactor: handle null user case in account API GET request

--- src/routes/(protected)/api/account/[id]/+server.ts
+++ src/routes/(protected)/api/account/[id]/+server.ts
@@ -5,13 +5,20 @@ import type { Set } from "$lib/database/documents/Set";
 import type { Folder } from "$lib/database/documents/Folder";
 
 export async function GET({ params, fetch }) {
-    const user: User = await idToDocument("users", params.id);
+    const user: User | null = await idToDocument("users", params.id);
+
+    if (!user) {
+        return json({
+            success: false,
+        });
+    }
+
     const sets: Set[] = await (await fetch(`/api/account/${params.id}/sets`)).json();
     const folders: Folder[] = await (await fetch(`/api/account/${params.id}/folders`)).json();
 
     return json({
-        name: user?.name,
-        image: user?.image,
+        name: user.name,
+        image: user.image,
         sets: sets,
         folders: folders,
     });
