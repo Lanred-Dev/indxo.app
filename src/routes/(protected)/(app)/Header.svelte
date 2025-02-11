cfd3069a419b1800a53dccd9e58dc78a00b53809
Landon Redmond <landon.redmond0@gmail.com>
2025-02-11 11:27:29 -0600

    fix: enhance search functionality in Header component with query validation and improved result display

--- src/routes/(protected)/(app)/Header.svelte
+++ src/routes/(protected)/(app)/Header.svelte
@@ -7,19 +7,58 @@
     let { user, sidebarVisible }: { user: any; sidebarVisible: Writable<boolean> } = $props();
 
     let searching: boolean = $state(false);
+    let searchQuery: string = $state("");
     let searchResults: (PublicUser | PublicSet | PublicFolder)[] = $state([]);
 
     async function search(event: Event) {
+        searchQuery = (event.target as HTMLInputElement)?.value;
+
+        // 3 is the minimum search query
+        if (searchQuery.length < 3) return;
+
         searchResults = await (
             await fetch("/api/search", {
                 method: "POST",
                 body: JSON.stringify({
-                    query: (event.target as HTMLInputElement)?.value,
+                    query: searchQuery,
                 }),
             })
         ).json();
     }
 </script>
+
+{#snippet searchResult({
+    name,
+    icon,
+    description,
+    subject,
+}: {
+    name: string;
+    icon?: string;
+    description?: string;
+    subject?: string;
+})}
+    <div class="flex items-center gap-1">
+        {#if icon}
+            <img class="size-6" src={icon} alt={name} />
+        {/if}
+
+        <div class="space-y-0.5">
+            <p>{name}</p>
+
+            {#if description || subject}
+                <p class="text-light text-sm">{description || subject}</p>
+            {/if}
+        </div>
+    </div>
+{/snippet}
+
+{#snippet searchCategory(name: string, icon: string)}
+    <div class="flex items-center gap-1">
+        <img class="size-6" src={icon} alt={name} />
+        <p>Search "<span class="max-w-12 text-ellipsis">{searchQuery}</span>" in {name}</p>
+    </div>
+{/snippet}
 
 <header
     class="relative top-0 flex w-full items-center justify-between border-b border-primary bg-accent-primary px-10 py-4"
@@ -54,15 +93,17 @@
     </div>
 
     <div
-        class="x-center primary top-full w-96 rounded-lg border border-primary"
-        style:display={searching ? "block" : "none"}
+        class="x-center primary top-full z-50 w-96 rounded-lg border border-primary"
+        style:display={searching && searchQuery.length > 0 ? "block" : "none"}
     >
-        <div class="p-4">
+        <div class="space-y-2 p-4">
             {#each searchResults as result}
-                <div>
-                    <p>{result.name}</p>
-                </div>
+                {@render searchResult(result)}
             {/each}
+
+            {@render searchCategory("Users", "/icons/navigation/Account.svg")}
+            {@render searchCategory("Sets", "/icons/navigation/Document.svg")}
+            {@render searchCategory("Folders", "/icons/navigation/Folder.svg")}
         </div>
     </div>
 </header>
