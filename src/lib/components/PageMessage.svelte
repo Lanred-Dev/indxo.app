ad0c5253f859f242a3d799ae1ca85a4277a87027
Lanred-Dev <76853738+Lanred-Dev@users.noreply.github.com>
2025-02-13 23:58:39 -0600

    feat: add PageMessage component for displaying basic messages with a title, text, and button

---
+++ src/lib/components/PageMessage.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    let { title, text, button }: { title: string; text: string; button: [string, string] } =
+        $props();
+</script>
+
+<div class="mb-7 block">
+    <p class="text-3xl font-bold">{title}</p>
+    <p class="text-light text-lg">{text}</p>
+</div>
+
+<a class="primary block w-fit" href={button[1]}>{button[0]}</a>
