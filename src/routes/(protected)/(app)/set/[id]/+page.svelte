6f8c53d4cf53a9b5297196f715ab02a03cb56b30
Lanred-Dev <76853738+Lanred-Dev@users.noreply.github.com>
2025-02-17 11:48:55 -0600

    feat: enhance term display with shuffle and cycle functionality; remove Term component

--- src/routes/(protected)/(app)/set/[id]/+page.svelte
+++ src/routes/(protected)/(app)/set/[id]/+page.svelte
@@ -1,9 +1,37 @@
 <script lang="ts">
     import PageMessage from "$lib/components/PageMessage.svelte";
+    import type { Term } from "$lib/database/documents/Set";
     import { onMount } from "svelte";
-    import Term from "./Term.svelte";
+    import { formatDistanceToNow } from "date-fns";
 
     let { data } = $props();
+
+    let terms: Term[] = $state(data.terms || []);
+    let showDescription: boolean = $state(false);
+    let currentTermIndex: number = $state(0);
+    let currentTerm: Term = $derived(terms[currentTermIndex]);
+
+    /**
+     * Shuffle the terms in the set. Resets the current term to the first one.
+     *
+     * @returns never
+     */
+    function shuffle() {
+        currentTermIndex = 0;
+        showDescription = false;
+        terms = terms.sort(() => Math.random() - 0.5);
+    }
+
+    /**
+     * Cycle through the terms in the set.
+     *
+     * @param direction The direction to cycle in. -1 for previous, 1 for next.
+     * @returns never
+     */
+    function cycle(direction: -1 | 1) {
+        showDescription = false;
+        currentTermIndex += direction;
+    }
 
     onMount(async () => {
         await fetch(`/api/documents/set/${data._id}/open`);
@@ -32,23 +60,59 @@
         <img
             src={data.owner?.image}
             alt={data.owner?.name}
-            class="size-9 rounded-full border border-primary"
+            class="size-12 rounded-full border border-primary"
         />
-        <p class="text-lg">{data.owner?.name}</p>
+
+        <div class="leading-tight">
+            <p class="text-lg font-bold">{data.owner?.name}</p>
+            <p class="text-light">
+                Created {formatDistanceToNow(data.created || new Date(), {
+                    includeSeconds: true,
+                    addSuffix: true,
+                })}
+            </p>
+        </div>
     </a>
 
-    <div class="mt-8 space-y-4">
-        <div class="space-y-1">
+    <div class="mt-6 space-y-4">
+        <div class="leading-tight">
             <h1 class="text-5xl font-bold">{data.name}</h1>
-            <p class="text-light text-xl">{data.subject}</p>
+            <p class="text-light text-2xl">{data.subject}</p>
         </div>
 
         <p class="text-lg">{data.description}</p>
     </div>
 
-    <div class="mt-8">
-        {#each data.terms || [] as term}
-            <Term {...term} />
-        {/each}
+    <div class="mt-16 w-3/5 space-y-4">
+        <div class="w-full">
+            <button
+                class="aspect-[1.9] w-full rounded-primary border border-primary bg-primary-200 shadow-2xl"
+                onclick={() => (showDescription = !showDescription)}
+            >
+                {#if showDescription}
+                    <p>{currentTerm.description}</p>
+                {:else}
+                    <p>{currentTerm.name}</p>
+                {/if}
+            </button>
+        </div>
+
+        <div class="flex-center relative w-full gap-4">
+            <button class="primary" disabled={currentTermIndex === 0} onclick={() => cycle(-1)}>
+                Previous
+            </button>
+
+            <p>{currentTermIndex + 1} / {terms.length}</p>
+
+            <button
+                class="primary"
+                disabled={currentTermIndex === terms.length - 1}
+                onclick={() => cycle(1)}
+            >
+                Next
+            </button>
+
+            <button class="y-center absolute right-0" onclick={() => shuffle()}> Shuffle </button>
+        </div>
     </div>
 {/if}
