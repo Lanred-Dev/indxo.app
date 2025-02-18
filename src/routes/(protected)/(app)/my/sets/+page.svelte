0607e791740619e9d4226eb8a4ab8935e6345be8
Landon Redmond <landon.redmond0@gmail.com>
2025-02-18 11:37:23 -0600

    feat: implement set card component and update sets page to display sets in a grid

--- src/routes/(protected)/(app)/my/sets/+page.svelte
+++ src/routes/(protected)/(app)/my/sets/+page.svelte
@@ -1,5 +1,13 @@
 <script lang="ts">
-    let { sets } = $props();
+    import Set from "./Card.svelte";
+
+    let { data } = $props();
 </script>
 
-<h1>All of your sets</h1>
+<h1 class="text-5xl font-bold">All of your sets</h1>
+
+<div class="flex w-full flex-wrap gap-2">
+    {#each data.sets as set}
+        <Set {...set} />
+    {/each}
+</div>
---
+++ src/routes/(protected)/(app)/my/sets/Card.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+    import type { PublicSet } from "$lib/database/documents/Set";
+
+    let { _id, name, subject, description, isPublic }: PublicSet = $props();
+</script>
+
+<div class="primary inline-block w-full border border-primary">
+    <a
+        class="flex h-full w-full flex-col justify-between gap-4 overflow-hidden px-8 py-6 [&>p]:leading-tight"
+        href="/set/{_id}"
+    >
+        <div class="w-full">
+            <p class="w-full overflow-hidden overflow-ellipsis text-2xl font-bold">{name}</p>
+            <p class="text-dark">{subject}</p>
+        </div>
+
+        <p class="text-light line-clamp-3 overflow-hidden overflow-ellipsis text-lg">
+            {description}
+        </p>
+    </a>
+</div>
