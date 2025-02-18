01b4e1086e201b500b0f21fb21f3c79296aac05f
Landon Redmond <landon.redmond0@gmail.com>
2025-02-18 11:05:03 -0600

    feat: update to fetch and return PublicSet data for opened sets

--- src/routes/(protected)/api/home/feed/your-recents/+server.ts
+++ src/routes/(protected)/api/home/feed/your-recents/+server.ts
@@ -1,8 +1,9 @@
 import { json } from "@sveltejs/kit";
 import idToDocument from "$lib/utils/idToDocument";
 import type { User } from "$lib/database/documents/User";
+import type { PublicSet } from "$lib/database/documents/Set.js";
 
-export async function GET({ locals }) {
+export async function GET({ locals, fetch }) {
     const user: User = await idToDocument("users", locals.userID);
     const openedSets = user.openedSets;
 
@@ -11,9 +12,17 @@ export async function GET({ locals }) {
     openedSets.reverse();
     openedSets.splice(10);
 
+    // Fetch all data for the sets
+    let sets: PublicSet[] = [];
+
+    openedSets.forEach(async (info) => {
+        const set: PublicSet = await (await fetch(`/api/set/${info[0]}`)).json();
+        sets.push(set);
+    });
+
     return json({
         type: "set",
         linkTo: "/my/recents/",
-        cards: openedSets,
+        cards: sets,
     });
 }
