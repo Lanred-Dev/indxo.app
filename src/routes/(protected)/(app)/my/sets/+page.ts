8368179dcd96bb2ad8c57a8f99c74d9e08209f32
Landon Redmond <landon.redmond0@gmail.com>
2025-02-18 12:06:13 -0600

    fix: change userID type from number to string in load function

--- src/routes/(protected)/(app)/my/sets/+page.ts
+++ src/routes/(protected)/(app)/my/sets/+page.ts
@@ -1,7 +1,7 @@
 import type { PublicSet } from "$lib/database/documents/Set";
 
 export async function load({ fetch, params }) {
-    const userID: number = await (await fetch("/api/account")).json();
+    const userID: string = await (await fetch("/api/account")).json();
     const sets: PublicSet[] = await (await fetch(`/api/account/${userID}/sets`)).json();
 
     return {
