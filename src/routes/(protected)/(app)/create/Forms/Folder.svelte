f7aaf301d52ea65ac6b12eee119e885da8ab532c
Landon Redmond <landon.redmond0@gmail.com>
2025-02-19 20:58:45 -0600

    feat: implement functionality to add sets in Folder component

--- src/routes/(protected)/(app)/create/Forms/Folder.svelte
+++ src/routes/(protected)/(app)/create/Forms/Folder.svelte
@@ -6,6 +6,14 @@
     let { stage }: { stage: "creation" | "setup" } = $props();
 
     let sets: PublicSet[] = $state([]);
+    let setsToAdd: PublicSet[] = $state([]);
+    let setsToAddValue: string = $derived.by(() => {
+        return json.stringify(setsToAdd);
+    });
+
+    function addSet() {
+        sets.push(setsToAdd);
+    }
 
     onMount(async () => {
         const userID: string = await (await fetch("/api/account")).json();
@@ -60,4 +68,17 @@
         placeholder="This folder contains all my study sets for..."
         classes="h-40"
     />
-{:else}{/if}
+{:else}
+    <FormInput id="folders" type="custom">
+        <div class="data" data-value={setsToAddValue}></div>
+
+        <div class="grid grid-cols-3 gap-4">
+            {#each sets as set}
+                <div class="w-full">
+                    <p>{set.name}</p>
+                    <button onclick={addSet(set._id)}>add</button>
+                </div>
+            {/each}
+        </div>
+    </FormInput>
+{/if}
