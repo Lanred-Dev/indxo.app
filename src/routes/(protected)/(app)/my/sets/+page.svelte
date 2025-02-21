01d78c7c6e6fe84dc0fa30731c321f95d6daad66
Landon Redmond <landon.redmond0@gmail.com>
2025-02-21 11:17:59 -0600

    refactor: rename Set component to SetCard and update import path

--- src/routes/(protected)/(app)/my/sets/+page.svelte
+++ src/routes/(protected)/(app)/my/sets/+page.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-    import Set from "./Card.svelte";
+    import SetCard from "$lib/components/Cards/Set.svelte";
 
     let { data } = $props();
 </script>
@@ -8,6 +8,6 @@
 
 <div class="mt-2 flex w-full flex-wrap gap-2">
     {#each data.sets as set}
-        <Set {...set} />
+        <SetCard {...set} />
     {/each}
 </div>
