aa92a3c5e546e1727bc42eb2ce9054466f11e45f
Landon Redmond <landon.redmond0@gmail.com>
2025-02-21 11:23:57 -0600

    feat: add Folder card component to display folder details

---
+++ src/lib/components/Cards/Folder.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    import type { PublicFolder } from "$lib/database/documents/Folder";
+
+    let { _id, name, description, isPublic }: PublicFolder = $props();
+</script>
+
+<div class="primary inline-block w-full border border-primary">
+    <a
+        class="flex h-full w-full flex-col justify-between gap-4 overflow-hidden px-8 py-6 [&>p]:leading-tight"
+        href="/folder/{_id}"
+    >
+        <p class="w-full overflow-hidden overflow-ellipsis text-xl font-bold">{name}</p>
+        <p class="text-light line-clamp-1 overflow-hidden overflow-ellipsis">{description}</p>
+    </a>
+</div>
