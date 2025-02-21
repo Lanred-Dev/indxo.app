42105df4f4ce9e8a00a340d0aae2b831c7bb21b7
Landon Redmond <landon.redmond0@gmail.com>
2025-02-21 11:23:42 -0600

    feat: add folders page to display user's folders

---
+++ src/routes/(protected)/(app)/my/folders/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    import FolderCard from "$lib/components/Cards/Folder.svelte";
+
+    let { data } = $props();
+</script>
+
+<h1 class="pl-3 text-3xl font-bold">All of your folders</h1>
+
+<div class="mt-2 flex w-full flex-wrap gap-2">
+    {#each data.folders as folder}
+        <FolderCard {...folder} />
+    {/each}
+</div>
