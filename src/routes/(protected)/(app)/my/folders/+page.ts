1f1195c657cc0596e8800ea8cb6ec05c3161ccd4
Landon Redmond <landon.redmond0@gmail.com>
2025-02-25 11:27:47 -0600

    fix: remove unused parameters from load function in folders and sets pages

--- src/routes/(protected)/(app)/my/folders/+page.ts
+++ src/routes/(protected)/(app)/my/folders/+page.ts
@@ -1,6 +1,6 @@
 import type { PublicFolder } from "$lib/database/documents/Folder";
 
-export async function load({ fetch, params }) {
+export async function load({ fetch }) {
     const userID: string = await (await fetch("/api/account")).json();
     const folders: PublicFolder[] = await (await fetch(`/api/account/${userID}/folders`)).json();
 
--- src/routes/(protected)/(app)/my/sets/+page.ts
+++ src/routes/(protected)/(app)/my/sets/+page.ts
@@ -1,6 +1,6 @@
 import type { PublicSet } from "$lib/database/documents/Set";
 
-export async function load({ fetch, params }) {
+export async function load({ fetch }) {
     const userID: string = await (await fetch("/api/account")).json();
     const sets: PublicSet[] = await (await fetch(`/api/account/${userID}/sets`)).json();
 
