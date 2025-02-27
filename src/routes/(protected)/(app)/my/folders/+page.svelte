c4b2589284f2a38516e83a315136d9464b8780ee
Landon Redmond <landon.redmond0@gmail.com>
2025-02-26 22:10:36 -0600

    feat: enhance folder and set headings with Quetine font

--- src/routes/(protected)/(app)/my/folders/+page.svelte
+++ src/routes/(protected)/(app)/my/folders/+page.svelte
@@ -4,7 +4,10 @@
     let { data } = $props();
 </script>
 
-<h1 class="pl-3 text-3xl font-bold">All of your folders</h1>
+<h1 class="flex flex-col gap-4 pl-3">
+    <span class="text-5xl font-bold">All of your</span>
+    <span class="font-Quetine pl-44 text-7xl">folders</span>
+</h1>
 
 <div class="mt-2 flex w-full flex-wrap gap-2">
     {#each data.folders as folder}
--- src/routes/(protected)/(app)/my/sets/+page.svelte
+++ src/routes/(protected)/(app)/my/sets/+page.svelte
@@ -4,9 +4,12 @@
     let { data } = $props();
 </script>
 
-<h1 class="pl-3 text-3xl font-bold">All of your sets</h1>
+<h1 class="flex flex-col gap-4 pl-3">
+    <span class="text-5xl font-bold">All of your</span>
+    <span class="font-Quetine pl-44 text-7xl">sets</span>
+</h1>
 
-<div class="mt-2 flex w-full flex-wrap gap-2">
+<div class="mt-16 flex w-full flex-wrap gap-2">
     {#each data.sets as set}
         <SetCard {...set} />
     {/each}
