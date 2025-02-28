1ce493d9dfee8ae8a8cc1cb14186c6b8b85fe7fb
Landon Redmond <landon.redmond0@gmail.com>
2025-02-27 22:04:23 -0600

    feat: add load function to redirect invalid creation URLs to /create/set

---
+++ src/routes/(protected)/(app)/create/[slug]/+page.server.ts
@@ -0,0 +1,9 @@
+// This page corrects the creation URL if its not /folder or /set.
+
+import { redirect } from "@sveltejs/kit";
+
+export function load({ params }) {
+    if (params.slug === "folder" || params.slug === "set") return;
+
+    throw redirect(308, `/create/set`);
+}
