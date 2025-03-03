e81a6edbd3fbd777cbba3ad8ed025c5f0d453a87
Landon Redmond <landon.redmond0@gmail.com>
2025-03-02 20:18:22 -0600

    feat: enhance set card display with subject and term count

--- src/routes/(protected)/(app)/my/[slug]/Cards/Set.svelte
+++ src/routes/(protected)/(app)/my/[slug]/Cards/Set.svelte
@@ -1,7 +1,26 @@
 <script lang="ts">
     import type { PublicSet } from "$lib/database/documents/Set";
 
-    let { name }: PublicSet = $props();
+    let { _id, name, subject, description, terms }: PublicSet = $props();
 </script>
 
-<p>{name}</p>
+<div class="primary inline-block w-full border border-primary">
+    <a
+        class="flex h-full w-full flex-col justify-between gap-4 overflow-hidden px-8 py-6 [&>p]:leading-tight"
+        href="/set/{_id}"
+    >
+        <div class="w-full">
+            <p class="w-full overflow-hidden overflow-ellipsis text-xl font-bold">{name}</p>
+
+            <div class="[&>p]:text-dark flex items-center gap-2 [&>p]:text-sm">
+                {#if subject.length > 0}
+                    <p>{subject}</p>
+                {/if}
+
+                <p>{terms.length} {terms.length === 1 ? "term" : "terms"}</p>
+            </div>
+        </div>
+
+        <p class="text-light line-clamp-1 overflow-hidden overflow-ellipsis">{description}</p>
+    </a>
+</div>
