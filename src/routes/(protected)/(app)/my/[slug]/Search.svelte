3c1684878d86a53d0c0062307211b6b528645334
Landon Redmond <landon.redmond0@gmail.com>
2025-03-02 20:18:44 -0600

    feat: add search input component

---
+++ src/routes/(protected)/(app)/my/[slug]/Search.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    export let searchQuery: string = "";
+</script>
+
+<div class="flex w-full items-center justify-between pb-16 pt-12">
+    <div data-input class="flex-center w-3/5 gap-2">
+        <img class="size-5" src="/icons/general/Search.svg" alt="Search" />
+
+        <input
+            class="w-full border-0 bg-transparent p-0 outline-none focus:ring-0"
+            type="text"
+            placeholder="Cant find something?"
+            bind:value={searchQuery}
+        />
+    </div>
+</div>
