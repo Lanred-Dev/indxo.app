6d974a4aedcc36458ebd4c7434a64a90494dda7f
Lanred-Dev <76853738+Lanred-Dev@users.noreply.github.com>
2025-03-03 21:25:20 -0600

    refactor: replace PublicUser type with SimpleUser in folder and set API endpoints

--- src/routes/(protected)/api/documents/folder/[id]/+server.ts
+++ src/routes/(protected)/api/documents/folder/[id]/+server.ts
@@ -3,7 +3,7 @@ import idToDocument from "$lib/utils/idToDocument";
 import type { Folder, PublicFolder } from "$lib/database/documents/Folder";
 import type { Set } from "$lib/database/documents/Set";
 import permissionCheck from "$lib/utils/permissionCheck";
-import type { PublicUser } from "$lib/database/documents/User";
+import type { SimpleUser } from "$lib/database/documents/User";
 
 export async function GET({ params, fetch, locals }) {
     const folder: Folder = await idToDocument("folders", params.id);
@@ -21,7 +21,7 @@ export async function GET({ params, fetch, locals }) {
 
     (folder as unknown as PublicFolder).sets = sets;
 
-    const owner: PublicUser = await (await fetch(`/api/account/${folder.owner}`)).json();
+    const owner: SimpleUser = await (await fetch(`/api/account/${folder.owner}/simple`)).json();
     (folder as unknown as PublicFolder).owner = owner;
 
     return json(folder);
--- src/routes/(protected)/api/documents/set/[id]/+server.ts
+++ src/routes/(protected)/api/documents/set/[id]/+server.ts
@@ -1,7 +1,7 @@
 import { json } from "@sveltejs/kit";
 import idToDocument from "$lib/utils/idToDocument";
 import type { PublicSet, Set } from "$lib/database/documents/Set";
-import type { PublicUser } from "$lib/database/documents/User";
+import type { SimpleUser } from "$lib/database/documents/User";
 import permissionCheck from "$lib/utils/permissionCheck";
 
 export async function GET({ params, locals, fetch }) {
@@ -11,7 +11,7 @@ export async function GET({ params, locals, fetch }) {
         return json({ success: false }, { status: 403 });
     }
 
-    const owner: PublicUser = await (await fetch(`/api/account/${set.owner}`)).json();
+    const owner: SimpleUser = await (await fetch(`/api/account/${set.owner}/simple`)).json();
     (set as unknown as PublicSet).owner = owner;
 
     return json(set);
