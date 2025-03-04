94a941a62f91420f507180623d754691b08e295d
Lanred-Dev <76853738+Lanred-Dev@users.noreply.github.com>
2025-03-04 11:39:21 -0600

    refactor: update group rendering to improve clarity and adjust layout

--- src/routes/(protected)/(app)/Sidebar.svelte
+++ src/routes/(protected)/(app)/Sidebar.svelte
@@ -4,17 +4,17 @@
     let { visible }: { visible: Writable<boolean> } = $props();
 </script>
 
-{#snippet group(name: string | null, links: { url: string; text: string; icon: string }[])}
+{#snippet group(links: { url: string; text: string; icon: string }[], name?: string)}
     <div>
         {#if name}
-            <p class="text-light mb-2">{name}...</p>
+            <p class="text-light mb-3">{name}</p>
         {/if}
 
-        <ul class="space-y-2">
+        <ul class="space-y-2.5">
             {#each links as { url, text, icon }}
                 <li>
                     <a class="flex items-center gap-2" href={url} data-sveltekit-reload>
-                        <img class="size-6" src={icon} alt={text} />
+                        <img class="size-7" src={icon} alt={text} />
                         <p class="text-lg leading-none">{text}</p>
                     </a>
                 </li>
@@ -24,24 +24,30 @@
 {/snippet}
 
 <div
-    class="relative w-[18%] min-w-fit space-y-10 border-r border-primary bg-primary-400 p-10"
+    class="relative w-[15%] min-w-fit space-y-10 border-r border-primary bg-primary-400 p-10"
     style="display: {$visible ? 'block' : 'none'};"
 >
-    {@render group(null, [
+    {@render group([
         { icon: "/icons/navigation/Home.svg", text: "Home", url: "/" },
         { icon: "/icons/navigation/Account.svg", text: "Account", url: "/account" },
     ])}
 
-    {@render group("My library", [
-        { icon: "/icons/navigation/Stars.svg", text: "Favorites", url: "/my/favorites" },
-        { icon: "/icons/navigation/Folder.svg", text: "Folders", url: "/my/folders" },
-        { icon: "/icons/navigation/Document.svg", text: "Study Sets", url: "/my/sets" },
-    ])}
+    {@render group(
+        [
+            { icon: "/icons/navigation/Stars.svg", text: "Favorites", url: "/my/favorites" },
+            { icon: "/icons/navigation/Folder.svg", text: "Folders", url: "/my/folders" },
+            { icon: "/icons/navigation/Document.svg", text: "Study Sets", url: "/my/sets" },
+        ],
+        "Your library"
+    )}
 
-    {@render group("Create new", [
-        { icon: "/icons/navigation/FolderPlus.svg", text: "Folder", url: "/create/folder" },
-        { icon: "/icons/navigation/DocumentPlus.svg", text: "Study Set", url: "/create/set" },
-    ])}
+    {@render group(
+        [
+            { icon: "/icons/navigation/FolderPlus.svg", text: "Folder", url: "/create/folder" },
+            { icon: "/icons/navigation/DocumentPlus.svg", text: "Study Set", url: "/create/set" },
+        ],
+        "Create a new"
+    )}
 
     <p class="text-dark absolute bottom-10 text-sm">Made for Savannah ❤️</p>
 </div>
