c3a0474b013937634bc83305fd92c3b49753b0c5
Lanred-Dev <76853738+Lanred-Dev@users.noreply.github.com>
2025-03-03 21:28:18 -0600

    fix: display message for sets with no terms

--- src/routes/(protected)/(app)/(documents)/set/[id]/StudyCards.svelte
+++ src/routes/(protected)/(app)/(documents)/set/[id]/StudyCards.svelte
@@ -54,67 +54,71 @@
     });
 </script>
 
-<div class="my-24 w-4/5 space-y-4">
-    <div class="w-full">
-        <button
-            class="relative aspect-[1.9] max-h-96 w-full rounded-primary border border-primary bg-primary-200 p-6 shadow-2xl"
-            onclick={() => (showDescription = !showDescription)}
-        >
-            {#if showDescription}
-                <p class="text-light x-center top-6">{currentTerm.name}</p>
-            {/if}
-
-            <p class="text-2xl">
+{#if actualTerms.length === 0}
+    <p class="my-24 text-center text-lg font-bold">This set has no terms</p>
+{:else}
+    <div class="my-24 w-4/5 space-y-4">
+        <div class="w-full">
+            <button
+                class="relative aspect-[1.9] max-h-96 w-full rounded-primary border border-primary bg-primary-200 p-6 shadow-2xl"
+                onclick={() => (showDescription = !showDescription)}
+            >
                 {#if showDescription}
-                    {currentTerm.description}
-                {:else}
-                    {currentTerm.name}
+                    <p class="text-light x-center top-6">{currentTerm.name}</p>
                 {/if}
-            </p>
-        </button>
-    </div>
 
-    <div class="relative w-full">
-        <div class="flex-center gap-1">
-            <button
-                class="primary group transition-opacity disabled:pointer-events-none disabled:opacity-50"
-                disabled={currentTermIndex === 0}
-                onclick={() => cycle(-1)}
-            >
-                <img
-                    class="relative size-6 transition-transform group-hover:-translate-x-0.5 group-hover:scale-105"
-                    src="/icons/general/LeftArrow.svg"
-                    alt="Previous"
-                />
-            </button>
-
-            <p class="w-20 text-center text-lg font-bold">
-                {currentTermIndex + 1}<span class="font-normal">/</span>{actualTerms.length}
-            </p>
-
-            <button
-                class="primary group transition-opacity disabled:pointer-events-none disabled:opacity-50"
-                disabled={currentTermIndex === actualTerms.length - 1}
-                onclick={() => cycle(1)}
-            >
-                <img
-                    class="relative size-6 transition-transform group-hover:translate-x-0.5 group-hover:scale-105"
-                    src="/icons/general/RightArrow.svg"
-                    alt="Next"
-                />
+                <p class="text-2xl">
+                    {#if showDescription}
+                        {currentTerm.description}
+                    {:else}
+                        {currentTerm.name}
+                    {/if}
+                </p>
             </button>
         </div>
 
-        <div class="flex-center y-center absolute right-8 gap-2">
-            {#if actualTerms.length > 1}
+        <div class="relative w-full">
+            <div class="flex-center gap-1">
                 <button
-                    class="transition-transform hover:rotate-[28deg] hover:scale-110 active:rotate-[180deg]"
-                    style="-webkit-animation-fill-mode: forwards; animation-fill-mode: forwards;"
-                    onclick={() => shuffle()}
+                    class="primary group transition-opacity disabled:pointer-events-none disabled:opacity-50"
+                    disabled={currentTermIndex === 0}
+                    onclick={() => cycle(-1)}
                 >
-                    <img class="size-6" src="/icons/general/Shuffle.svg" alt="Shuffle" />
+                    <img
+                        class="relative size-6 transition-transform group-hover:-translate-x-0.5 group-hover:scale-105"
+                        src="/icons/general/LeftArrow.svg"
+                        alt="Previous"
+                    />
                 </button>
-            {/if}
+
+                <p class="w-20 text-center text-lg font-bold">
+                    {currentTermIndex + 1}<span class="font-normal">/</span>{actualTerms.length}
+                </p>
+
+                <button
+                    class="primary group transition-opacity disabled:pointer-events-none disabled:opacity-50"
+                    disabled={currentTermIndex === actualTerms.length - 1}
+                    onclick={() => cycle(1)}
+                >
+                    <img
+                        class="relative size-6 transition-transform group-hover:translate-x-0.5 group-hover:scale-105"
+                        src="/icons/general/RightArrow.svg"
+                        alt="Next"
+                    />
+                </button>
+            </div>
+
+            <div class="flex-center y-center absolute right-8 gap-2">
+                {#if actualTerms.length > 1}
+                    <button
+                        class="transition-transform hover:rotate-[28deg] hover:scale-110 active:rotate-[180deg]"
+                        style="-webkit-animation-fill-mode: forwards; animation-fill-mode: forwards;"
+                        onclick={() => shuffle()}
+                    >
+                        <img class="size-6" src="/icons/general/Shuffle.svg" alt="Shuffle" />
+                    </button>
+                {/if}
+            </div>
         </div>
     </div>
-</div>
+{/if}
