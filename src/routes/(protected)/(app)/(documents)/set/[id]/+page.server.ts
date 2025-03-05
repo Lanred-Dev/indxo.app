bd289b611bb69a21d4e30c57d5fd42c9e0f6cc70
Lanred-Dev <76853738+Lanred-Dev@users.noreply.github.com>
2025-03-04 21:22:33 -0600

    feat: add favorite functionality

--- src/routes/(protected)/(app)/(documents)/set/[id]/+page.server.ts
+++ src/routes/(protected)/(app)/(documents)/set/[id]/+page.server.ts
@@ -1,5 +1,6 @@
-import type { PublicSet } from "$lib/database/documents/Set";
+import type { PublicSet, Set } from "$lib/database/documents/Set";
 import permissionCheck from "$lib/utils/permissionCheck.js";
+import type { ObjectId } from "mongodb";
 
 export async function load({ fetch, params }) {
     const response = await fetch(`/api/documents/set/${params.id}`);
@@ -11,11 +12,15 @@ export async function load({ fetch, params }) {
     }
 
     const userID: string = await (await fetch("/api/account")).json();
+    const userFavorites: ObjectId[] = await (
+        await fetch(`/api/account/${userID}/favorites`)
+    ).json();
     const set: PublicSet = await response.json();
 
     return {
         set,
+        didFavorite: userFavorites.includes(set._id),
         permission: true,
-        hasEditPermission: permissionCheck(set as any, userID, true),
+        hasEditPermission: permissionCheck(set as unknown as Set, userID, true),
     };
 }
