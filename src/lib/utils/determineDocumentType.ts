988dc6cf13f1d49ad2fe72859ed8dfb34385b41c
Landon Redmond <landon.redmond0@gmail.com>
2025-03-06 11:54:16 -0600

    refactor: improve determineDocumentType to make it a more usable util

---
+++ src/lib/utils/determineDocumentType.ts
@@ -0,0 +1,17 @@
+/**
+ * Determines the type of document.
+ *
+ * @param document The document to determine the type of.
+ * @returns Either "account", "folder", or "set".
+ */
+export default function determineDocumentType(document: {
+    icon?: string;
+    image?: string;
+    subject?: string;
+}): "account" | "folder" | "set" {
+    if ("icon" in document) return "folder";
+    if ("subject" in document) return "set";
+    if ("image" in document) return "account";
+
+    return "account";
+}
