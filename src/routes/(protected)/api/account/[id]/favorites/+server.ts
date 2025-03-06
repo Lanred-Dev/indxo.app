8e638f08b5681f946e976eda3f25a937bb55b077
Landon Redmond <landon.redmond0@gmail.com>
2025-03-06 16:18:32 -0600

    refactor: update favorites retrieval to include PublicSet and PublicFolder data

--- src/routes/(protected)/api/account/[id]/favorites/+server.ts
+++ src/routes/(protected)/api/account/[id]/favorites/+server.ts
@@ -1,6 +1,8 @@
 import { json } from "@sveltejs/kit";
 import idToDocument from "$lib/utils/idToDocument";
 import type { User } from "$lib/database/documents/User";
+import type { PublicSet, Set } from "$lib/database/documents/Set";
+import type { Folder, PublicFolder } from "$lib/database/documents/Folder";
 
 export async function GET({ params }) {
     const user: User | null = await idToDocument("users", params.id);
@@ -11,5 +13,12 @@ export async function GET({ params }) {
         });
     }
 
-    return json(user.favorites);
+    const favorites: (PublicSet | PublicFolder)[] = [];
+
+    for (const [id, type] of user.favorites) {
+        const document: Set | Folder = await idToDocument(`${type}s`, id);
+        favorites.push(document as unknown as PublicSet | PublicFolder);
+    }
+
+    return json(favorites);
 }
