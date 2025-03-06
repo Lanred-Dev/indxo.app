f7576b61843016486d5bc4033d666ba55a6f7e7f
Lanred-Dev <76853738+Lanred-Dev@users.noreply.github.com>
2025-03-05 21:12:45 -0600

    refactor: update EditableList component to use EditableListItem and enhance item properties and action buttons handling

--- src/lib/components/EditableList/EditableList.svelte
+++ src/lib/components/EditableList/EditableList.svelte
@@ -1,24 +1,46 @@
 <script lang="ts">
-    import BasicItem from "./BasicItem.svelte";
+    import EditableListItem, { type actionButton, type property } from "./EditableListItem.svelte";
     import { onMount, type Component } from "svelte";
     import { twMerge } from "tailwind-merge";
 
     let {
         classes,
         startingItems = 1,
-        properties = { name: "", description: "" },
-        ListComponent = BasicItem,
+        properties = [
+            {
+                id: "name",
+                type: "input",
+                placeholder: "Name",
+            },
+            {
+                id: "description",
+                type: "textarea",
+                placeholder: "Description",
+            },
+        ],
+        actionButtons = [
+            {
+                image: ["/icons/general/Trash.svg", "Delete"],
+                onClick: onItemDeleteClicked,
+            },
+        ],
+        isDraggable = false,
         addText = "Add item",
+        ListComponent = EditableListItem,
     }: {
         classes?: string;
         startingItems?: number;
-        properties?: { [key: string]: any };
-        ListComponent?: Component<{ index: number } & any, {}, "">;
+        properties?: property[];
+        actionButtons?: actionButton[];
+        ListComponent?: Component<{ index: number } & any, {}, "properties">;
         addText?: string;
+        isDraggable?: boolean;
     } = $props();
 
     let listContainer: HTMLElement;
     let items: { id: number; [key: string]: any }[] = $state([]);
+    let draggingElementID: number = $state(-1);
+    let draggingOverElementID: number = $state(-1);
 
     /**
      * Adds an item to the list.
@@ -26,98 +48,75 @@
      * @returns never
      */
     function addItem() {
+        let actionButtonsClone: actionButton[] | null = [...actionButtons];
+
+        if (isDraggable) {
+            if (!Array.isArray(actionButtonsClone)) actionButtonsClone = [];
+
+            // Adding it at the start of the array ensures the order of the buttons stays correct
+            actionButtonsClone.unshift({
+                classes: "cursor-move",
+                image: ["/icons/general/Drag.svg", "Drag"],
+                onMouseDown: onDragStart,
+                onMouseUp: onDragEnd,
+            });
+        }
+
         items.push({
             id: items.length,
-            ...properties,
+            properties: properties,
+            actionButtons: actionButtonsClone,
         });
     }
 
     /**
-     * Removes an item from the list.
+     * Handles when a list item's delete button is clicked and removes the item from the list.
      *
-     * @param id The id of the item to remove.
+     * @param event The click event.
      * @returns never
      */
-    function removeItem(id: number) {
-        items = items.filter((item) => item.id !== id);
-    }
+    function onItemDeleteClicked(event: MouseEvent) {
+        const editableListItem: HTMLElement = (event.target as HTMLElement).closest(
+            ".editableListItem"
+        )!;
 
-    /**
-     * Handles when a dropdown item is clicked and updates the current value.
-     *
-     * @param event
-     * @returns never
-     */
-    function itemDeleteClicked(event: MouseEvent) {
-        const target = event.target as HTMLElement;
-        const editableListItem = target.closest(".editableListItem");
+        if (!editableListItem) return;
 
-        if (editableListItem) {
-            const id: string = editableListItem.getAttribute("data-id")!;
-            removeItem(parseInt(id));
-        }
-    }
+        const id: number = parseInt(editableListItem.getAttribute("data-id")!);
+        items.splice(id, 1);
 
-    /**
-     * Sets up event listeners for the items. Also removes any existing event listeners.
-     *
-     * @returns never
-     */
-    function setupEventListeners() {
-        removeEventListeners();
-
-        const listItems: NodeListOf<HTMLElement> =
-            listContainer.querySelectorAll(".editableListItem")!;
-
-        listItems.forEach((item: HTMLElement) => {
-            const deleteButton: HTMLButtonElement = item.querySelector(".deleteButton")!;
-            deleteButton.addEventListener("click", itemDeleteClicked);
+        items.forEach((item, index) => {
+            item.id = index;
         });
     }
 
-    /**
-     * Removes event listeners from the items.
-     *
-     * @returns never
-     */
-    function removeEventListeners() {
-        const listItems: NodeListOf<HTMLElement> =
-            listContainer.querySelectorAll(".editableListItem")!;
+    function onDragStart(event: MouseEvent) {}
 
-        listItems.forEach((item: HTMLElement) => {
-            const deleteButton: HTMLButtonElement = item.querySelector(".deleteButton")!;
-            deleteButton.removeEventListener("click", itemDeleteClicked);
-        });
-    }
+    function onDragEnd() {}
 
     onMount(() => {
+        // Add the requested number of items to start with
         for (let index: number = 0; index < startingItems; index++) {
             addItem();
         }
-
-        setupEventListeners();
-
-        const observer: MutationObserver = new MutationObserver((mutationList) => {
-            mutationList.forEach((mutation) => {
-                if (mutation.type === "childList") {
-                    setupEventListeners();
-                }
-            });
-        });
-
-        observer.observe(listContainer, { childList: true });
-
-        return () => {
-            observer.disconnect();
-            removeEventListeners();
-        };
     });
 </script>
 
 <div class={twMerge("editableList space-y-5", classes)} bind:this={listContainer}>
-    {#each items as item, index (item.id)}
-        <ListComponent {index} {...properties} />
-    {/each}
+    <ol class="space-y-5">
+        {#each items as { id, actionButtons }}
+            <!--The `li` element is used for MOST of the dragging features. However, if using a custom list component, the `ListComponent` must support action buttons in order for dragging to work!-->
+            <li
+                class="transition-all {draggingElementID === id
+                    ? 'opacity-35'
+                    : draggingOverElementID === id
+                      ? '[&>.editableListItem]:!border-alert [&>.editableListItem]:!border [&>.editableListItem]:!border-dashed'
+                      : ''}"
+            >
+                <ListComponent {id} bind:properties={items[id].properties} {actionButtons} />
+            </li>
+        {/each}
+    </ol>
 
     <button class="primary" onclick={addItem} type="button">{addText}</button>
 </div>
