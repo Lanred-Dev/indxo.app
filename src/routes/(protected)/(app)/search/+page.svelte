3a4037012aa45eda528ced9482b99af79e7b7afd
Landon Redmond <landon.redmond0@gmail.com>
2025-03-07 10:17:30 -0600

    feat: add title to search results page

--- src/routes/(protected)/(app)/search/+page.svelte
+++ src/routes/(protected)/(app)/search/+page.svelte
@@ -1,2 +1,18 @@
 <script lang="ts">
+    import determineWording from "$lib/utils/determineWording";
+
+    let { data } = $props();
 </script>
+
+<svelte:head>
+    <title>Results for "{data.query}"</title>
+</svelte:head>
+
+<div>
+    <p class="text-light text-xl leading-tight">
+        Searching in {typeof data.returnOnly === "string"
+            ? determineWording(data.returnOnly)
+            : "all"}
+    </p>
+    <h1 class="text-5xl font-bold leading-none">Results for "{data.query}"</h1>
+</div>
