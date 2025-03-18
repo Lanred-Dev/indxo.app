468e7f395a82ee9c614695f2ba0eaf3e0b9ae951
Landon Redmond <landon.redmond0@gmail.com>
2025-03-18 12:01:35 -0500

    refactor: update sets retrieval to use PublicSet type and include owner information

--- src/routes/(protected)/api/account/[id]/sets/+server.ts
+++ src/routes/(protected)/api/account/[id]/sets/+server.ts
@@ -1,11 +1,11 @@
 import { json } from "@sveltejs/kit";
 import idToDocument from "$lib/utils/idToDocument";
-import type { Set } from "$lib/database/documents/Set";
+import type { PublicSet, Set } from "$lib/database/documents/Set";
 import type { User } from "$lib/database/documents/User";
 
 export async function GET({ params }) {
     const user: User = await idToDocument("users", params.id);
-    const sets: Set[] = [];
+    const sets: PublicSet[] = [];
 
     for (const id of user.sets) {
         const set: Set = await idToDocument("sets", id);
@@ -14,7 +14,13 @@ export async function GET({ params }) {
             continue;
         }
 
-        sets.push(set);
+        (set as unknown as PublicSet).owner = {
+            _id: user._id,
+            name: user.name,
+            image: user.image,
+        };
+
+        sets.push(set as unknown as PublicSet);
     }
 
     return json(sets);
