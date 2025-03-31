c5d0c8fc8792c2496bdcbc04383d66f99a57d1c2
Lanred-Dev <76853738+Lanred-Dev@users.noreply.github.com>
2025-03-31 10:43:34 -0500

    refactor: update session expiration handling and user type definitions

--- src/lib/auth/session.ts
+++ src/lib/auth/session.ts
@@ -2,7 +2,7 @@ import { encodeBase32, encodeHexLowerCase } from "@oslojs/encoding";
 import { sha256 } from "@oslojs/crypto/sha2";
 import idToDocument from "$lib/utils/idToDocument";
 import type { Session } from "$lib/database/documents/Session";
-import type { PublicUser } from "$lib/database/documents/User";
+import type { SimpleUser, User } from "$lib/database/documents/User";
 import { ObjectId, type Collection } from "mongodb";
 import { loadCollection } from "$lib/database/mongo";
 import { milliseconds } from "date-fns";
@@ -14,7 +14,7 @@ export async function createSession(token: string, user: ObjectId): Promise<Sess
     const session: Session = {
         _id: new ObjectId(sessionID),
         user: new ObjectId(user.toString()),
-        expires: new Date(Date.now() + 1000 * 60 * 60 * 24 * 30),
+        expires: Date.now() + milliseconds({ days: 30 }),
     };
 
     await sessions.insertOne(session);
@@ -24,26 +24,34 @@ export async function createSession(token: string, user: ObjectId): Promise<Sess
 
 export async function validateToken(
     token: string
-): Promise<{ user: PublicUser; session: Session } | null> {
+): Promise<{ user: SimpleUser; session: Session } | null> {
     const sessionID = encodeHexLowerCase(sha256(new TextEncoder().encode(token)));
     const session: Session = (await idToDocument("sessions", sessionID)) as Session;
 
     if (!session) return null;
 
-    if (Date.now() >= session.expires.getTime()) {
+    if (Date.now() >= session.expires) {
         deleteSession(token);
         return null;
     }
 
     // If the session is within 15 days of expiration, extend it by 30.
-    if (Date.now() >= session.expires.getTime() - milliseconds({ days: 15 })) {
-        session.expires = new Date(Date.now() + milliseconds({ days: 30 }));
+    if (Date.now() >= session.expires - milliseconds({ days: 15 })) {
+        session.expires = Date.now() + milliseconds({ days: 30 });
 
         await sessions.updateOne({ _id: session._id }, { $set: { expires: session.expires } });
     }
 
-    const user = await idToDocument("users", session.user);
-    return { user, session };
+    const user: User = await idToDocument("users", session.user);
+
+    return {
+        user: {
+            _id: user._id,
+            name: user.name,
+            image: user.image,
+        },
+        session,
+    };
 }
 
 export async function deleteSession(token: string) {
