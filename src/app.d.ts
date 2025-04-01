2e8d6903c65f2c367ed0be286a39822b8894e85c
Landon Redmond <landon.redmond0@gmail.com>
2025-04-01 12:49:00 -0500

    refactor: update user type to SimpleUserWithEmail in app.d.ts

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,11 +1,11 @@
 import type { Session } from "$lib/database/documents/Session";
-import type { SimpleUser } from "$lib/database/documents/User";
+import type { SimpleUserWithEmail } from "$lib/database/documents/User";
 
 declare global {
     namespace App {
         interface Locals {
             session: Session | null;
-            user: SimpleUser;
+            user: SimpleUserWithEmail;
         }
     }
 }
