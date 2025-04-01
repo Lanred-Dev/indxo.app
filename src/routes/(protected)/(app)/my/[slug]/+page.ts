586901b94e68c2026119f05925e0a638890562f8
Landon Redmond <landon.redmond0@gmail.com>
2025-04-01 12:05:24 -0500

    fix: correct redirect logic for invalid slugs in user routes

--- src/routes/(protected)/(app)/my/[slug]/+page.ts
+++ src/routes/(protected)/(app)/my/[slug]/+page.ts
@@ -4,9 +4,8 @@ import { redirect } from "@sveltejs/kit";
 
 export async function load({ fetch, params }) {
     // This corrects the URL if its not /folders, /sets, /favorites.
-    if (params.slug !== "folders" && params.slug !== "sets" && params.slug !== "favorites") {
+    if (params.slug !== "folders" && params.slug !== "sets" && params.slug !== "favorites")
         throw redirect(308, `/my/sets`);
-    }
 
     const userID: string = await (await fetch("/api/account")).json();
     const documents: (PublicFolder | PublicSet)[] = await (
