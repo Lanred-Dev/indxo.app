c0b5501d09fa5989ae53b4160dc2be1e38c3f739
Lanred-Dev <76853738+Lanred-Dev@users.noreply.github.com>
2025-04-02 23:35:56 -0500

    feat: add utility function to generate random IDs

---
+++ src/lib/utils/generateRandomID.ts
@@ -0,0 +1,17 @@
+const CHARACTERS: string = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+/**
+ * Generates a random ID consisting of uppercase letters and digits.
+ *
+ * @param length The length of the random ID to generate. Default is 12 characters.
+ * @returns
+ */
+export default function generateRandomID(length: number = 12): string {
+    let id: string = "";
+
+    for (let index: number = 0; index < length; index++) {
+        id += CHARACTERS.charAt(Math.floor(Math.random() * 36));
+    }
+
+    return id;
+}
