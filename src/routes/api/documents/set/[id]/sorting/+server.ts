e233a74047def1bd5fdc1acf207435b843883fce
Landon Redmond <landon.redmond0@gmail.com>
2025-04-07 12:45:15 -0500

    feat: add sorting retrieval and update endpoints for document sets

---
+++ src/routes/api/documents/set/[id]/sorting/+server.ts
@@ -0,0 +1,17 @@
+import { error, json } from "@sveltejs/kit";
+import idToDocument from "$lib/utils/idToDocument";
+import type { User } from "$lib/database/documents/User";
+import type { Set } from "$lib/database/documents/Set";
+import permissionCheck from "$lib/utils/permissionCheck";
+
+export async function GET({ params, locals, fetch }) {
+    if (!locals.session) error(401, "Unauthorized.");
+
+    const set: Set = await (await fetch(`/api/documents/set/${params.id}`)).json();
+
+    if (!permissionCheck(set, locals.user._id))
+        error(403, "You do not have permission to view this set.");
+
+    const user: User = await idToDocument("users", locals.user._id);
+    return json(user.meta[params.id]?.sorting || []);
+}
---
+++ src/routes/api/documents/set/[id]/sorting/update/+server.ts
@@ -0,0 +1,33 @@
+import { error, json } from "@sveltejs/kit";
+import idToDocument from "$lib/utils/idToDocument";
+import type { User } from "$lib/database/documents/User";
+import type { Set } from "$lib/database/documents/Set";
+import permissionCheck from "$lib/utils/permissionCheck";
+import type { Collection } from "mongodb";
+import { loadCollection } from "$lib/database/mongo";
+
+const users: Collection<User> = loadCollection("accounts", "users");
+
+export async function GET({ params, request, locals, fetch }) {
+    if (!locals.session) error(401, "Unauthorized.");
+
+    const set: Set = await (await fetch(`/api/documents/set/${params.id}`)).json();
+
+    if (!permissionCheck(set, locals.user._id))
+        error(403, "You do not have permission to view this set.");
+
+    users.updateOne(
+        {
+            _id: locals.user._id,
+        },
+        {
+            $set: {
+                [`meta.${params.id}.sorting`]: await request.json(),
+            },
+        }
+    );
+
+    return new Response(null, {
+        status: 204,
+    });
+}
