32a69d9e419108ff82990fccde31cc294267fe3f
Landon Redmond <landon.redmond0@gmail.com>
2025-04-09 11:56:22 -0500

    feat: create Account component for displaying user information

---
+++ src/routes/(app)/(documents)/Account.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+    import type { SimpleUser } from "$lib/database/documents/User";
+    import { formatDistanceToNow } from "date-fns";
+
+    let { owner, created }: { owner: SimpleUser; created: number } = $props();
+</script>
+
+<a class="flex gap-2" href="/account/{owner._id}">
+    <img src={owner.image} alt={owner.name} class="size-11 rounded-full border border-primary" />
+
+    <div class="[&>p]:leading-tight">
+        <p class="text-dark text-sm leading-snug">Created by</p>
+        <p class="text-lg font-bold">{owner.name}</p>
+        <p class="text-light text-sm">
+            {formatDistanceToNow(created ?? Date.now(), {
+                includeSeconds: true,
+                addSuffix: true,
+            })}
+        </p>
+    </div>
+</a>
