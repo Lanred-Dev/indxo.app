dc751a7069447e3a9eca448e87cb9c591327875b
Landon Redmond <landon.redmond0@gmail.com>
2025-04-09 11:56:30 -0500

    refactor: replace owner details with Account component in Info.svelte

--- src/routes/(app)/(documents)/set/[id]/(_index)/Info.svelte
+++ src/routes/(app)/(documents)/set/[id]/(_index)/Info.svelte
@@ -1,29 +1,12 @@
 <script lang="ts">
     import type { PublicSet } from "$lib/database/documents/Set";
-    import { formatDistanceToNow } from "date-fns";
+    import Account from "../../../Account.svelte";
 
     let { description, owner, created }: PublicSet = $props();
 </script>
 
 <div class="mb-12 mt-20 space-y-6">
-    <a class="flex gap-2" href="/account/{owner._id}">
-        <img
-            src={owner.image}
-            alt={owner.name}
-            class="size-11 rounded-full border border-primary"
-        />
-
-        <div class="[&>p]:leading-tight">
-            <p class="text-dark text-sm leading-snug">Created by</p>
-            <p class="text-lg font-bold">{owner.name}</p>
-            <p class="text-light text-sm">
-                {formatDistanceToNow(created ?? new Date(), {
-                    includeSeconds: true,
-                    addSuffix: true,
-                })}
-            </p>
-        </div>
-    </a>
+    <Account {owner} {created} />
 
     <p class="text-lg">{description}</p>
 </div>
