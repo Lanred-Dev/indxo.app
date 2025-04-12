3c2c3f1a9df87c4db33ed4ee7f3cb3a78b55717b
Landon Redmond <landon.redmond0@gmail.com>
2025-04-12 18:20:32 -0500

    feat: add section visibility toggle

--- src/routes/(app)/(protected)/my/[slug]/List.svelte
+++ src/routes/(app)/(protected)/my/[slug]/List.svelte
@@ -4,46 +4,58 @@
     import determineDocumentType from "$lib/utils/determineDocumentType";
     import Card from "$lib/components/Card.svelte";
     import determineWording from "$lib/utils/determineWording";
+    import { slide } from "svelte/transition";
 
     export type group = [(PublicFolder | PublicSet)[], string | null];
 
     let groups: group[] = $props();
+    let visible: boolean = $state.raw(true);
 </script>
 
 <div class="w-full space-y-10">
     {#each groups as [items, name]}
         <div class="list-primary">
-            {#if typeof name === "string" && name.length > 0}
-                <p class="list-title">{name}</p>
-            {/if}
+            <button class="flex-center gap-1" onclick={() => (visible = !visible)}>
+                {#if typeof name === "string" && name.length > 0}
+                    <p class="list-title">{name}</p>
+                {/if}
 
-            <ul class="list flex-col">
-                {#each items as item}
-                    <li>
-                        {#if determineDocumentType(item) === "folder"}
-                            <Card
-                                name={item.name}
-                                description={item.description}
-                                url={`/folder/${item._id}`}
-                                icon={(item as PublicFolder).icon}
-                                meta={[
-                                    `${(item as PublicFolder).sets.length} ${determineWording((item as PublicFolder).sets.length === 1 ? "set" : "sets")}`,
-                                ]}
-                            />
-                        {:else if determineDocumentType(item) === "set"}
-                            <Card
-                                name={item.name}
-                                description={item.description}
-                                url={`/set/${item._id}`}
-                                meta={[
-                                    (item as PublicSet).subject,
-                                    `${(item as PublicSet).terms.length} ${determineWording((item as PublicSet).terms.length === 1 ? "term" : "terms")}`,
-                                ]}
-                            />
-                        {/if}
-                    </li>
-                {/each}
-            </ul>
+                <img
+                    class="size-4"
+                    src="/icons/general/{visible ? 'UpChevron' : 'DownChevron'}.svg"
+                    alt="Arrow {visible ? 'up' : 'down'}"
+                />
+            </button>
+
+            {#if visible}
+                <ul class="list flex-col" in:slide out:slide>
+                    {#each items as item}
+                        <li>
+                            {#if determineDocumentType(item) === "folder"}
+                                <Card
+                                    name={item.name}
+                                    description={item.description}
+                                    url={`/folder/${item._id}`}
+                                    icon={(item as PublicFolder).icon}
+                                    meta={[
+                                        `${(item as PublicFolder).sets.length} ${determineWording((item as PublicFolder).sets.length === 1 ? "set" : "sets")}`,
+                                    ]}
+                                />
+                            {:else if determineDocumentType(item) === "set"}
+                                <Card
+                                    name={item.name}
+                                    description={item.description}
+                                    url={`/set/${item._id}`}
+                                    meta={[
+                                        (item as PublicSet).subject,
+                                        `${(item as PublicSet).terms.length} ${determineWording((item as PublicSet).terms.length === 1 ? "term" : "terms")}`,
+                                    ]}
+                                />
+                            {/if}
+                        </li>
+                    {/each}
+                </ul>
+            {/if}
         </div>
     {/each}
 </div>
