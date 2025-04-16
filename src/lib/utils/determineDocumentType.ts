921cd22f3204b072ff8a51c0057d2650215b11a7
Landon Redmond <landon.redmond0@gmail.com>
2025-04-16 11:52:32 -0500

    refactor: update return types and improve logic in determineDocumentType function

--- src/lib/utils/determineDocumentType.ts
+++ src/lib/utils/determineDocumentType.ts
@@ -2,17 +2,18 @@
  * Determines the type of document.
  *
  * @param document The document to determine the type of.
- * @returns Either "account", "folder", or "set".
+ * @returns Either "user", "folder", or "set".
  */
 export default function determineDocumentType(document: {
     icon?: string;
     image?: string;
     subject?: string;
     [key: string]: any;
-}): "account" | "folder" | "set" {
-    if ("icon" in document) return "folder";
-    if ("subject" in document) return "set";
-    if ("image" in document) return "account";
+}): "user" | "folder" | "set" {
+    console.log(document);
+    if ("subject" in document && (document.subject ?? "").trim().length > 0) return "set";
+    if ("image" in document && (document.image ?? "").trim().length > 0) return "user";
+    if ("icon" in document && (document.icon ?? "").trim().length > 0) return "folder";
 
-    return "account";
+    return "set";
 }
