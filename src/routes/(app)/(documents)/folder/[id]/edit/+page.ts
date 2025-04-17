fa2d086b9ff98c96b1a8a301d8c92e19a25a6345
Lanred-Dev <76853738+Lanred-Dev@users.noreply.github.com>
2025-04-16 20:34:29 -0500

    feat: add edit page for folder

---
+++ src/routes/(app)/(documents)/folder/[id]/edit/+page.ts
@@ -0,0 +1,11 @@
+import type { PublicFolder } from "$lib/database/documents/Folder.js";
+import { redirect } from "@sveltejs/kit";
+
+export async function load({ fetch, params }) {
+    const response = await fetch(`/api/documents/folder/${params.id}`);
+
+    if (response.status === 403) redirect(307, `/folder/${params.id}`);
+
+    const folder: PublicFolder = await response.json();
+    return folder;
+}
