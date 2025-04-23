f7203385afa03da8f2db46197bd5166c61593c41
Lanred-Dev <76853738+Lanred-Dev@users.noreply.github.com>
2025-04-22 23:16:57 -0500

    fix: rename value to checkboxValue for clarity in Checkbox component

--- src/lib/components/Checkbox.svelte
+++ src/lib/components/Checkbox.svelte
@@ -5,7 +5,7 @@
         classes,
         text,
         icons = ["/icons/general/Check.svg", "/icons/general/X.svg"],
-        value = $bindable(false),
+        value: checkboxValue = $bindable(false),
     }: {
         classes?: string;
         icons?: [string, string];
@@ -14,12 +14,16 @@
     } = $props();
 </script>
 
-<div class={twMerge("checkbox relative", classes)} data-value={value}>
-    <button class="input-primary" onclick={() => (value = !value)} type="button">
-        <img class="size-7" src={value ? icons[0] : icons[1]} alt={value ? "True" : "False"} />
+<div class={twMerge("checkbox relative", classes)} data-value={checkboxValue}>
+    <button class="input-primary" onclick={() => (checkboxValue = !checkboxValue)} type="button">
+        <img
+            class="size-7"
+            src={checkboxValue ? icons[0] : icons[1]}
+            alt={checkboxValue ? "True" : "False"}
+        />
 
         {#if text}
-            <span class="select-none">{value ? text[0] : text[1]}</span>
+            <span class="select-none">{checkboxValue ? text[0] : text[1]}</span>
         {/if}
     </button>
 </div>
