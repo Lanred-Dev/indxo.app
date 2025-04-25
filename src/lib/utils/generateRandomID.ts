bde07203b6af06b5199932e75db292da05045dab
Lanred-Dev <76853738+Lanred-Dev@users.noreply.github.com>
2025-04-25 11:34:24 -0500

    refactor: improve random ID generation and update documentation

--- src/lib/utils/generateRandomID.ts
+++ src/lib/utils/generateRandomID.ts
@@ -4,14 +4,16 @@ const CHARACTERS: string = "abcdefghijklmnopqrstuvwxyz0123456789";
  * Generates a random ID consisting of uppercase letters and digits.
  *
  * @param length The length of the random ID to generate. Default is 12 characters.
- * @returns
+ * @returns The generated random ID as a string.
  */
 export default function generateRandomID(length: number = 12): string {
     let id: string = "";
+    let bytes = new Uint8Array(length);
+    crypto.getRandomValues(bytes);
 
-    for (let index: number = 0; index < length; index++) {
-        id += CHARACTERS.charAt((Math.random() * 36) | 0);
+    for (let i = 0; i < length; i++) {
+        id += CHARACTERS[bytes[i] % CHARACTERS.length];
     }
 
-    return id;
+    return id.toLowerCase();
 }
