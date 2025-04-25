a2cf9126a23fcb346af3422c92b41c46235d465d
Lanred-Dev <76853738+Lanred-Dev@users.noreply.github.com>
2025-04-25 11:05:10 -0500

    feat: add layout component to manage sidebar visibility on navigation for study modes

---
+++ src/routes/(app)/(documents)/set/[id]/(modes)/+layout.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    import { afterNavigate } from "$app/navigation";
+    import { getContext } from "svelte";
+
+    let { children } = $props();
+
+    const sidebarVisible: { visible: boolean } = getContext("sidebarVisible");
+
+    afterNavigate((navigation) => {
+        if (navigation.to?.url.pathname.includes("/sort")) {
+            sidebarVisible.visible = false;
+        } else if (window.innerWidth > 786) {
+            sidebarVisible.visible = true;
+        }
+    });
+</script>
+
+{@render children?.()}
