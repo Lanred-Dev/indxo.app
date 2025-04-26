4dd3bef216a721a3806d23408b4fb7454641e3db
Lanred-Dev <76853738+Lanred-Dev@users.noreply.github.com>
2025-04-26 11:57:45 -0500

    feat: enhance form submission button with loading state

--- src/lib/components/Form/FormSubmit.svelte
+++ src/lib/components/Form/FormSubmit.svelte
@@ -1,7 +1,41 @@
 <script lang="ts">
+    import { onMount } from "svelte";
+    import Loader from "../Loader.svelte";
+
     let { text }: { text: string } = $props();
+
+    let submitting: boolean = $state.raw(false);
+    let button: HTMLButtonElement;
+
+    onMount(() => {
+        const form: HTMLFormElement | null = button.closest("form");
+
+        if (!form) return;
+
+        const observer: MutationObserver = new MutationObserver((mutations) => {
+            mutations.forEach(() => {
+                submitting = form.getAttribute("data-submitting") === "true";
+            });
+        });
+
+        observer.observe(form, {
+            attributes: true,
+            attributeFilter: ["data-submitting"],
+        });
+
+        return () => observer.disconnect();
+    });
 </script>
 
-<button type="submit" class="button-attention w-full">
-    {@html text}
+<button
+    type="submit"
+    class="button-attention flex-center w-full"
+    disabled={submitting}
+    bind:this={button}
+>
+    {#if submitting}
+        <Loader size={1.75} color="#000000" />
+    {:else}
+        {@html text}
+    {/if}
 </button>
