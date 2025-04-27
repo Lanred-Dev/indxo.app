4f1194fdd698cd1f13d9faae4b02f33327ffe981
Lanred-Dev <76853738+Lanred-Dev@users.noreply.github.com>
2025-04-27 09:38:17 -0500

    fix: update load function to use parent for user data retrieval

--- src/routes/(app)/(protected)/my/[slug]/+page.ts
+++ src/routes/(app)/(protected)/my/[slug]/+page.ts
@@ -2,14 +2,14 @@ import type { PublicFolder } from "$lib/database/documents/Folder";
 import type { PublicSet } from "$lib/database/documents/Set.js";
 import { redirect } from "@sveltejs/kit";
 
-export async function load({ fetch, params }) {
+export async function load({ fetch, params, parent }) {
     // This corrects the URL if its not /folders, /sets, /favorites.
     if (params.slug !== "folders" && params.slug !== "sets" && params.slug !== "favorites")
         redirect(308, `/my/sets`);
 
-    const userID: string = await (await fetch("/api/user")).json();
+    const { user } = await parent();
     const documents: (PublicFolder | PublicSet)[] = await (
-        await fetch(`/api/user/${userID}/${params.slug}`)
+        await fetch(`/api/user/${user._id}/${params.slug}`)
     ).json();
 
     return {
