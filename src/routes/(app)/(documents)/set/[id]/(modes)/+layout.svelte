ac4f214d2fea1203fc50ed82161b767dcfa16a98
Landon Redmond <landon.redmond0@gmail.com>
2025-05-02 12:56:01 -0500

    feat: add back to study set button for modes

--- src/routes/(app)/(documents)/set/[id]/(modes)/+layout.svelte
+++ src/routes/(app)/(documents)/set/[id]/(modes)/+layout.svelte
@@ -2,13 +2,14 @@
     import { beforeNavigate } from "$app/navigation";
     import { getContext, onMount } from "svelte";
     import type { SidebarContext } from "../../../../+layout.svelte";
+    import determineWording from "$lib/utils/determineWording";
 
-    let { children } = $props();
+    let { data, children } = $props();
 
     const sidebar: SidebarContext = getContext("sidebar");
 
     function checkIfShouldHideSidebar(url: string) {
-        if (url.includes("/sort")) {
+        if (url.includes("/sort") || url.includes("/matching")) {
             sidebar.visible = false;
         } else if (window.innerWidth > 786) {
             sidebar.visible = true;
@@ -23,5 +24,9 @@
         checkIfShouldHideSidebar(window.location.pathname);
     });
 </script>
+
+<div class="mb-10">
+    <a href="/set/{data.id}">Back to {determineWording("set")}</a>
+</div>
 
 {@render children?.()}
