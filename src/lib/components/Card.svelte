9d2a795a6d7ed8a75d1457afffbb7c8bab699c03
Lanred-Dev <76853738+Lanred-Dev@users.noreply.github.com>
2025-05-19 10:56:39 -0500

    fix: remove useless classes from Card component

--- src/lib/components/Card.svelte
+++ src/lib/components/Card.svelte
@@ -18,52 +18,48 @@
 
 <!--NOTE: For some reason not having the block class can cause some weird issues-->
 <a
-    class="container-primary container-clay relative block w-full px-6 py-5"
+    class="container-primary container-clay block w-full px-6 py-5"
     style:--clay-color="color-mix(in srgb, var(--color-attention) 13%, #ffffff)"
     style:--clay-color-dark-percentage="95%"
     href={url}
 >
-    <div class="relative z-[1] w-full">
-        {#if info.length > 0}
-            <ol class="mb-3 flex flex-wrap items-center gap-x-3 gap-y-1.5">
-                {#each info as { text, image }}
-                    <li
-                        class="flex-center container-primary bg-attention-light gap-1 rounded-full border-0 shadow-xs {image
-                            ? 'pr-2 pl-1'
-                            : 'px-2'} py-1"
+    {#if info.length > 0}
+        <ol class="mb-3 flex flex-wrap items-center gap-x-3 gap-y-1.5">
+            {#each info as { text, image }}
+                <li
+                    class="flex-center container-primary bg-attention-light gap-1 rounded-full border-0 shadow-xs {image
+                        ? 'pr-2 pl-1'
+                        : 'px-2'} py-1"
+                >
+                    {#if image}
+                        <img class="size-5 rounded-full" src={image} alt={text} />
+                    {/if}
+
+                    <p
+                        class="max-w-28 overflow-x-hidden overflow-y-visible leading-tight text-nowrap text-ellipsis"
                     >
-                        {#if image}
-                            <img class="size-5 rounded-full" src={image} alt={text} />
-                        {/if}
+                        {text}
+                    </p>
+                </li>
+            {/each}
+        </ol>
+    {/if}
 
-                        <p
-                            class="max-w-28 overflow-x-hidden overflow-y-visible leading-tight text-nowrap text-ellipsis"
-                        >
-                            {text}
-                        </p>
-                    </li>
-                {/each}
-            </ol>
+    <div class="flex-center gap-1">
+        {#if icon}
+            <img src={icon} alt="Folder" class="size-8" />
         {/if}
 
-        <div class="flex-center gap-1">
-            {#if icon}
-                <img src={icon} alt="Folder" class="size-8" />
-            {/if}
-
-            <p
-                class="w-full max-w-full overflow-x-hidden text-2xl leading-none font-bold text-wrap text-ellipsis"
-            >
-                {name}
-            </p>
-        </div>
-
-        {#if description && description.length > 0}
-            <p
-                class="text-light mt-0.5 w-full overflow-x-hidden overflow-y-visible text-lg leading-none text-ellipsis"
-            >
-                {description}
-            </p>
-        {/if}
+        <p
+            class="w-full max-w-full overflow-hidden text-2xl leading-none font-bold text-wrap text-ellipsis"
+        >
+            {name}
+        </p>
     </div>
+
+    {#if description && description.length > 0}
+        <p class="text-light mt-0.5 w-full overflow-hidden text-lg leading-none text-ellipsis">
+            {description}
+        </p>
+    {/if}
 </a>
