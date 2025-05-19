8c03e080d86e59248620d0c41a8d66c33d5a77c9
Lanred-Dev <76853738+Lanred-Dev@users.noreply.github.com>
2025-05-19 10:59:49 -0500

    fix: only make terms editable if user owns the set

--- src/routes/(app)/(documents)/set/[id]/(_index)/Terms.svelte
+++ src/routes/(app)/(documents)/set/[id]/(_index)/Terms.svelte
@@ -1,38 +1,52 @@
 <script lang="ts">
     import EditableList from "$lib/components/Form/EditableList/EditableList.svelte";
     import type { Term } from "$lib/database/documents/Set";
-    import type { SortingTerm } from "$lib/database/documents/User";
     import determineWording from "$lib/utils/determineWording";
 
-    let { saved, terms }: { saved: SortingTerm[]; terms: Term[] } = $props();
+    let { terms, hasPermission }: { terms: Term[]; hasPermission: boolean } = $props();
 </script>
 
 <div class="list-primary mt-16">
     <p class="list-title">{determineWording("cards")}</p>
 
     <div class="list-primary">
-        <EditableList
-            addText="Add {determineWording('card')}"
-            isDraggable={true}
-            items={terms.map(({ _id, term, definition }) => ({
-                _id,
-                properties: {
-                    term,
-                    definition,
-                },
-            }))}
-            properties={[
-                {
-                    _id: "term",
-                    type: "input",
-                    placeholder: "Term",
-                },
-                {
-                    _id: "definition",
-                    type: "textarea",
-                    placeholder: "Definition",
-                },
-            ]}
-        />
+        {#if hasPermission}
+            <EditableList
+                addText="Add {determineWording('card')}"
+                isDraggable={true}
+                items={terms.map(({ _id, term, definition }) => ({
+                    _id,
+                    properties: {
+                        term,
+                        definition,
+                    },
+                }))}
+                properties={[
+                    {
+                        _id: "term",
+                        type: "input",
+                        placeholder: "Term",
+                    },
+                    {
+                        _id: "definition",
+                        type: "textarea",
+                        placeholder: "Definition",
+                    },
+                ]}
+            />
+        {:else}
+            <ol class="flex flex-col gap-4">
+                {#each terms as { term, definition }, listID}
+                    <li class="container-primary flex">
+                        <p class="mr-3 text-lg font-bold">#{listID + 1}</p>
+
+                        <div>
+                            <p class="text-lg font-bold">{term}</p>
+                            <p class="mt-0.5 text-lg leading-none">{definition}</p>
+                        </div>
+                    </li>
+                {/each}
+            </ol>
+        {/if}
     </div>
 </div>
