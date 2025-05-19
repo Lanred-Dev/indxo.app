eab35d7e875c59ace4ab073d50024c2919983c2b
Landon Redmond <landon.redmond0@gmail.com>
2025-05-19 13:09:38 -0500

    fix: ensure redirect status codes are correct

--- src/routes/(app)/(documents)/set/[id]/(modes)/+layout.ts
+++ src/routes/(app)/(documents)/set/[id]/(modes)/+layout.ts
@@ -4,11 +4,11 @@ import { redirect } from "@sveltejs/kit";
 export async function load({ params, fetch }) {
     const response: Response = await fetch(`/api/documents/set/${params.id}`);
 
-    if (response.status === 403) redirect(403, `/set/${params.id}`);
+    if (response.status === 403) redirect(307, `/set/${params.id}`);
 
     const set: PublicSet = await response.json();
 
-    if (set.terms.length === 0) redirect(302, `/set/${params.id}`);
+    if (set.terms.length === 0) redirect(303, `/set/${params.id}`);
 
     return set;
 }
