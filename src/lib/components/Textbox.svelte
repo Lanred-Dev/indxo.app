d5d8f9a21af1b4cb96f057c5d15605f14653fa30
Lanred-Dev <76853738+Lanred-Dev@users.noreply.github.com>
2025-06-16 10:54:58 -0500

    Add textbox component

---
+++ src/lib/components/Textbox.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+    let {
+        value = $bindable(""),
+        multiline = false,
+        maxlength = Infinity,
+        ...properties
+    }: {
+        value?: string;
+        multiline?: boolean;
+        maxlength?: number;
+        [key: string]: any;
+    } = $props();
+
+    let attributes = $derived({
+        class: ["input-primary field-sizing-content resize-none", multiline && "h-40"],
+        maxlength,
+        "data-max": value.length === maxlength ? true : undefined,
+        ...properties,
+    });
+</script>
+
+{#if multiline}
+    <textarea bind:value {...attributes}></textarea>
+{:else}
+    <input bind:value {...attributes} />
+{/if}
