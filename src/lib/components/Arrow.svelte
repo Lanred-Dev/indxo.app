c3f7433f8aef9fac6dccb78104af47f7288d4d75
Lanred-Dev <76853738+Lanred-Dev@users.noreply.github.com>
2025-06-19 11:09:50 -0500

    Add Arrow component with customizable states for directional icons

---
+++ src/lib/components/Arrow.svelte
@@ -0,0 +1,22 @@
+<script lang="ts" module>
+    export enum ArrowState {
+        left = "M11.03 3.97a.75.75 0 0 1 0 1.06l-6.22 6.22H21a.75.75 0 0 1 0 1.5H4.81l6.22 6.22a.75.75 0 1 1-1.06 1.06l-7.5-7.5a.75.75 0 0 1 0-1.06l7.5-7.5a.75.75 0 0 1 1.06 0Z",
+        right = "M12.97 3.97a.75.75 0 0 1 1.06 0l7.5 7.5a.75.75 0 0 1 0 1.06l-7.5 7.5a.75.75 0 1 1-1.06-1.06l6.22-6.22H3a.75.75 0 0 1 0-1.5h16.19l-6.22-6.22a.75.75 0 0 1 0-1.06Z",
+        up = "M11.47 2.47a.75.75 0 0 1 1.06 0l7.5 7.5a.75.75 0 1 1-1.06 1.06l-6.22-6.22V21a.75.75 0 0 1-1.5 0V4.81l-6.22 6.22a.75.75 0 1 1-1.06-1.06l7.5-7.5Z",
+        down = "M12 2.25a.75.75 0 0 1 .75.75v16.19l6.22-6.22a.75.75 0 1 1 1.06 1.06l-7.5 7.5a.75.75 0 0 1-1.06 0l-7.5-7.5a.75.75 0 1 1 1.06-1.06l6.22 6.22V3a.75.75 0 0 1 .75-.75Z",
+    }
+</script>
+
+<script lang="ts">
+    let {
+        state = ArrowState.down,
+        ...properties
+    }: {
+        state: ArrowState;
+        [key: string]: unknown;
+    } = $props();
+</script>
+
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" {...properties}>
+    <path class="transition-[d] duration-200" fill-rule="evenodd" d={state} clip-rule="evenodd" />
+</svg>
