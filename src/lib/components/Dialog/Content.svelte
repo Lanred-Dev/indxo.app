eb37ada9b9f13c7fe688529f45f159e71ca3e2c4
Lanred-Dev <76853738+Lanred-Dev@users.noreply.github.com>
2025-06-19 11:09:21 -0500

    Refactor Dialog component layout for improved responsiveness and styling

--- src/lib/components/Dialog/Content.svelte
+++ src/lib/components/Dialog/Content.svelte
@@ -17,11 +17,13 @@
         class="fixed top-0 left-0 z-50 h-full w-full backdrop-blur-sm backdrop-brightness-50"
         transition:fade={{ duration: 100 }}
     >
-        <div
-            class="container-primary x-center y-center flex-center relative z-50 w-fit min-w-[20%] flex-col px-8 py-8"
-            in:fly={{ y: 20, duration: 200 }}
-        >
-            {@render children()}
+        <div class="x-center y-center flex-center w-full px-5 md:px-0">
+            <div
+                class="container-primary flex-center relative w-fit min-w-[20%] flex-col px-4 py-6 text-lg md:px-8 md:py-10"
+                in:fly={{ y: 20, duration: 200 }}
+            >
+                {@render children()}
+            </div>
         </div>
     </div>
 {/if}
