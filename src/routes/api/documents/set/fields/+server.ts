5ddd114ea4b14fa15239f72630c1e949ad42908c
Lanred-Dev <76853738+Lanred-Dev@users.noreply.github.com>
2025-06-28 01:42:37 -0500

    Remove unused API endpoint files for folder and set fields

--- src/routes/api/documents/set/fields/+server.ts
+++
@@ -1,9 +0,0 @@
-import { setFields } from "$lib/documents";
-import { ResponseCodes, ResponseMessages } from "$lib/utils/apiResponses";
-import { error, json } from "@sveltejs/kit";
-
-export async function GET({ locals }) {
-    if (!locals.session) error(ResponseCodes.Unauthorized, ResponseMessages.Unauthorized);
-
-    return json(setFields);
-}
