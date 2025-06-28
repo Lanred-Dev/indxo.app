c55e917a9514f29b7d5d7fdf1589e89b51629568
Lanred-Dev <76853738+Lanred-Dev@users.noreply.github.com>
2025-06-28 01:38:38 -0500

    Refactor Textbox component to improve value binding and class handling

--- src/lib/components/Textbox.svelte
+++ src/lib/components/Textbox.svelte
@@ -1,20 +1,27 @@
 <script lang="ts">
+    import type { ClassValue } from "svelte/elements";
+
     let {
-        value = $bindable(""),
+        value = $bindable(),
         multiline = false,
         maxlength = Infinity,
+        class: className,
         ...properties
     }: {
         value?: string;
         multiline?: boolean;
         maxlength?: number;
+        class?: ClassValue;
         [key: string]: any;
     } = $props();
 
     let attributes = $derived({
-        class: ["input-primary field-sizing-content resize-none w-full", multiline && "h-40"],
+        class: [
+            `input-primary field-sizing-content resize-none w-full ${className}`,
+            multiline && "h-40",
+        ],
         maxlength,
-        "data-max": value.length === maxlength ? true : undefined,
+        "data-max": value?.length === maxlength ? true : undefined,
         ...properties,
     });
 </script>
