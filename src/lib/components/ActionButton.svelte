77b85873789deeb69c1b902a25a63e4c2bb7a503
Lanred-Dev <76853738+Lanred-Dev@users.noreply.github.com>
2025-07-03 13:27:57 -0500

    Add ActionButton component with image and text properties

---
+++ src/lib/components/ActionButton.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+    import type { Component } from "svelte";
+
+    interface Properties {
+        image: string | { Component: Component<any>; properties?: Record<string, unknown> };
+        text: string;
+        onclick: (...args: any[]) => void;
+    }
+
+    let { image, text, onclick }: Properties = $props();
+</script>
+
+<button type="button" aria-labelledby={text} {onclick}>
+    {#if typeof image === "string"}
+        <img class="size-6" src={image} alt={text} />
+    {:else}
+        <image.Component class="size-6" {...image.properties} />
+    {/if}
+</button>
