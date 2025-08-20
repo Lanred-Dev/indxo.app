5c1d978b82ad995ac3d1ec2a871076a89be9c07d
Landon Redmond <landon.redmond0@gmail.com>
2025-08-19 22:03:18 -0500

    Improve about component

---
+++ src/routes/(app)/[id]/(index)/(set)/(_index)/Breakdown.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+    import { getContext } from "svelte";
+    import type { DocumentContext } from "../../+page.svelte";
+    import { format } from "date-fns";
+    import { Wording } from "$lib/utils/wording";
+
+    let document: DocumentContext = getContext("document");
+</script>
+
+{#snippet info(icon: string, title: string, text: string)}
+    <div class="flex flex-wrap items-center justify-between gap-x-5 gap-y-1.5">
+        <div class="flex items-center gap-2">
+            <img class="size-6 rounded-full" src={icon} alt={title} />
+            <p>{title}</p>
+        </div>
+
+        <p class="font-semibold">{text}</p>
+    </div>
+{/snippet}
+
+<div class="mt-15">
+    <div class="flex w-full flex-col gap-y-3">
+        {@render info(
+            "/icons/general/Terms.svg",
+            "Terms",
+            `${document.terms.length} ${document.terms.length === 1 ? Wording.term : Wording.terms}`
+        )}
+
+        {@render info(
+            "/icons/general/Created.svg",
+            "Created on",
+            format(document.created, "dd/MM/yyyy")
+        )}
+
+        {@render info(document.owner.picture, "Created by", document.owner.name)}
+    </div>
+
+    {#if document.description && document.description.length > 0}
+        <p class="break-word mt-8 text-lg">{document.description}</p>
+    {/if}
+</div>
