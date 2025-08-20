c4f6f449b42cf9716132cf14faab52453b3d98af
Landon Redmond <landon.redmond0@gmail.com>
2025-08-19 21:57:56 -0500

    Allow action buttons to show text

--- src/lib/components/ActionButton.svelte
+++ src/lib/components/ActionButton.svelte
@@ -1,23 +1,32 @@
 <script lang="ts" generics="C extends Component<any>">
     import type { Component, ComponentProps } from "svelte";
 
-    interface Properties {
+    let {
+        image,
+        text,
+        showText = false,
+        ...properties
+    }: {
         image: { properties?: ComponentProps<C>; Component?: C; url?: string };
         text: string;
+        showText?: boolean;
         onclick: (...args: any[]) => void;
         [key: string]: unknown;
-    }
+    } = $props();
 
-    let { image, text, ...properties }: Properties = $props();
     const { class: imageClassName = "", ...imageProperties } = $derived(image.properties ?? {}) as {
         [key: string]: unknown;
     };
 </script>
 
-<button type="button" aria-labelledby={text} {...properties}>
+<button type="button" aria-label={text} {...properties}>
     {#if "url" in image}
         <img class={["size-6", imageClassName]} src={image.url} alt={text} {...imageProperties} />
     {:else}
         <image.Component class={["size-6", imageClassName]} {...imageProperties} />
+    {/if}
+
+    {#if showText}
+        <span class="text-sm">{text}</span>
     {/if}
 </button>
