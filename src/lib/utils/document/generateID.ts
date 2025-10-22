154441b0bac00c6bf1b2c2ceeed3352199425b3d
Landon Redmond <landon.redmond0@gmail.com>
2025-10-22 11:13:21 -0500

    Fix variable name casing for consistency in generateDocumentID function

--- src/lib/utils/document/generateID.ts
+++ src/lib/utils/document/generateID.ts
@@ -1,7 +1,7 @@
 import { DocumentType } from "$lib/documents";
 import randomArrayEntry from "../randomArrayEntry";
 
-const characters: string = "abcdefghijklmnopqrstuvwxyz0123456789";
+const CHARACTERS: string = "abcdefghijklmnopqrstuvwxyz0123456789";
 let startingCharacters: string[]; // A prefix is used to determine the type, so this prevents documents from being mislabeled if no prefix is provided
 
 /**
@@ -14,17 +14,14 @@ let startingCharacters: string[]; // A prefix is used to determine the type, so 
 export default function generateDocumentID(length: number, documentType?: string): string {
     // The DocumentType enum may not first be available on runtime
     if (!startingCharacters)
-        startingCharacters = characters
-            .split("")
-            .filter(
-                (character: string) =>
-                    !Object.values(DocumentType).includes(character as DocumentType)
-            );
+        startingCharacters = CHARACTERS.split("").filter(
+            (character: string) => !Object.values(DocumentType).includes(character as DocumentType)
+        );
 
     let id: string = "";
 
     for (let index: number = 0; index < length - (documentType ? 1 : 0); index++)
-        id += randomArrayEntry(index === 0 ? startingCharacters : characters);
+        id += randomArrayEntry(index === 0 ? startingCharacters : CHARACTERS);
 
     return `${documentType ?? ""}${id}`;
 }
