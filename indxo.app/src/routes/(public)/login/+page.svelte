f32e767dec6a8f0761f5accbe629071df4a314e9
Lanred-Dev <76853738+Lanred-Dev@users.noreply.github.com>
2025-10-31 22:16:55 -0500

    Fix login link

--- indxo.app/src/routes/(public)/login/+page.svelte
+++ indxo.app/src/routes/(public)/login/+page.svelte
@@ -2,7 +2,7 @@
     <title>Login</title>
 </svelte:head>
 
-<a class="button-primary x-center y-center" href="/api/auth/google">
+<a class="button-primary x-center y-center" href="/api/auth/google" data-sveltekit-reload>
     <img class="size-7" src="/icons/Google.svg" alt="Google" />
     Sign in with Google
 </a>
