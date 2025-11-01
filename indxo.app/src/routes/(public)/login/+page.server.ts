d13ba3170a8b09d3be99c5a4398c7226449eb1dc
Lanred-Dev <76853738+Lanred-Dev@users.noreply.github.com>
2025-11-01 01:14:02 -0500

    Move some login functionality to form action

--- indxo.app/src/routes/(public)/login/+page.server.ts
+++ indxo.app/src/routes/(public)/login/+page.server.ts
@@ -1,5 +1,31 @@
+import { google } from "$lib/server/auth/oauth";
 import { ResponseCodes } from "$lib/utils/apiResponses";
 import { redirect } from "@sveltejs/kit";
+import { generateCodeVerifier, generateState } from "arctic";
+
+export const actions = {
+    default: async ({ cookies }) => {
+        const state = generateState();
+        const verifier = generateCodeVerifier();
+        const url = google.createAuthorizationURL(state, verifier, ["openid", "profile", "email"]);
+
+        cookies.set("state", state, {
+            path: "/",
+            httpOnly: true,
+            maxAge: 60000,
+            sameSite: "lax",
+        });
+
+        cookies.set("verifier", verifier, {
+            path: "/",
+            httpOnly: true,
+            maxAge: 60000,
+            sameSite: "lax",
+        });
+
+        redirect(ResponseCodes.Redirect, url);
+    },
+};
 
 export function load({ locals }) {
     if (locals.session) redirect(ResponseCodes.Redirect, "/");
--- indxo.app/src/routes/(public)/login/+page.svelte
+++ indxo.app/src/routes/(public)/login/+page.svelte
@@ -1,8 +1,20 @@
+<script lang="ts">
+    import { enhance } from "$app/forms";
+</script>
+
 <svelte:head>
     <title>Login</title>
 </svelte:head>
 
-<a class="button-primary x-center y-center" href="/api/auth/google" data-sveltekit-reload>
-    <img class="size-7" src="/icons/Google.svg" alt="Google" />
-    Sign in with Google
-</a>
+<div class="cover-screen flex-center page-title mb-0 flex-col gap-6">
+    <h1 class="title">Welcome to indxo.app</h1>
+
+    <div class="row w-fit text-xl">
+        <form method="POST" use:enhance>
+            <button class="button-primary">
+                <img class="size-7" src="/icons/Google.svg" alt="Google" />
+                Sign in with Google
+            </button>
+        </form>
+    </div>
+</div>
