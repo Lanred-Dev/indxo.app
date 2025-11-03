a9c9f04b9a42d65958c6453c55edc9fff9614f92
Landon Redmond <landon.redmond0@gmail.com>
2025-11-03 11:28:19 -0600

    Add error handling to ExpandableImage

--- indxo.app/src/lib/components/Image/Expandable.svelte
+++ indxo.app/src/lib/components/Image/Expandable.svelte
@@ -9,18 +9,27 @@
         class: ClassValue;
         [key: string]: any;
     } = $props();
+
+    let didImageFailToLoad: boolean = $state.raw(false);
 </script>
 
 <Dialog>
-    <DialogTrigger class="group relative">
-        <img class={className} {...properties} />
+    {#if !didImageFailToLoad}
+        <DialogTrigger class="group relative">
+            <img class={className} {...properties} onerror={() => (didImageFailToLoad = true)} />
 
-        <img
-            class="absolute top-2 right-2 size-8 translate-y-2 opacity-0 transition-[translate,opacity] group-hover:translate-y-0 group-hover:opacity-100"
-            src="/icons/general/Expand.svg"
-            alt="Expand"
-        />
-    </DialogTrigger>
+            <img
+                class="absolute top-2 right-2 size-8 translate-y-2 opacity-0 transition-[translate,opacity] group-hover:translate-y-0 group-hover:opacity-100"
+                src="/icons/general/Expand.svg"
+                alt="Expand"
+            />
+        </DialogTrigger>
+    {:else}
+        <div class={["flex-center flex-col", className]} {...properties}>
+            <img class="size-8" src="/icons/general/Error.svg" alt="Error" />
+            <p class="text-sm">Failed to load image.</p>
+        </div>
+    {/if}
 
     <DialogContent class="min-h-30! rounded-none! p-0!">
         <DialogTrigger class="button-primary absolute top-3 right-3 rounded-full p-1.5! shadow-md">
