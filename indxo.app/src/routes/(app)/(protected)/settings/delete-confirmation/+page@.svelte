6b75bb8252b2ccd5070bdb848b50605335302097
Landon Redmond <landon.redmond0@gmail.com>
2025-11-02 23:51:50 -0600

    Add delete account confirmation page

---
+++ indxo.app/src/routes/(app)/(protected)/settings/delete-confirmation/+page@.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+    import { goto } from "$app/navigation";
+    import type { SessionContext } from "$lib/utils/global";
+    import { getContext } from "svelte";
+
+    const session: SessionContext = getContext("session");
+</script>
+
+<svelte:head>
+    <title>Delete Account</title>
+</svelte:head>
+
+<div class="cover-screen flex-center page-title mb-0 flex-col gap-6">
+    <div class="text-center">
+        <h1 class="title">Are you sure you want to delete your account?</h1>
+        <p class="description">This action is irreversible.</p>
+    </div>
+
+    <div class="row w-fit text-xl">
+        <button
+            class="button-attention clay-alert"
+            onclick={async () => {
+                const response = await fetch(`/api/user/${session.user._id}`, {
+                    method: "DELETE",
+                });
+
+                if (!response.ok) {
+                    alert("Failed to delete account.");
+                    return;
+                }
+
+                goto("/login");
+            }}>Yes, delete my account</button
+        >
+
+        <a href="/settings" class="button-primary">No, take me back</a>
+    </div>
+</div>
