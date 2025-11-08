617566a5749921da533a55864efb02b0808659d7
Landon Redmond <landon.redmond0@gmail.com>
2025-11-08 13:19:28 -0600

    Fix class name for list container in Editable component

--- indxo.app/src/lib/components/Lists/Editable/Content.svelte
+++ indxo.app/src/lib/components/Lists/Editable/Content.svelte
@@ -7,7 +7,7 @@
 </script>
 
 <div class="list-primary">
-    <ol class="list-container relative flex-col">
+    <ol class="relative container flex-col">
         {#each editableList.items as { index, _id, fields, isDraggable } (index)}
             <EditableListItem {index} {_id} bind:fields {isDraggable} />
         {/each}
