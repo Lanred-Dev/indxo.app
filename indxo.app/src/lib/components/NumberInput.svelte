41bb354267c85708ccd8eaa18c85de361d7d3873
Lanred-Dev <76853738+Lanred-Dev@users.noreply.github.com>
2025-11-08 23:17:00 -0600

    Add NumberInput component

---
+++ indxo.app/src/lib/components/NumberInput.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+    import type { ClassValue } from "svelte/elements";
+    import Chevron, { ChevronState } from "./Icons/Chevron.svelte";
+
+    let {
+        value = $bindable(0),
+        max = Infinity,
+        min = -Infinity,
+        class: className,
+        ...properties
+    }: {
+        value?: number;
+        max?: number;
+        min?: number;
+        class?: ClassValue;
+        [key: string]: any;
+    } = $props();
+</script>
+
+<div
+    class="input-primary flex-center gap-3"
+    role="spinbutton"
+    tabindex="0"
+    aria-valuenow={value}
+    aria-valuemin={min}
+    aria-valuemax={max}
+>
+    <p>{value}</p>
+
+    <div class="flex md:flex-col">
+        <button onclick={() => (value = Math.min(value + 1, max))} class="size-3">
+            <Chevron state={ChevronState.up} class="h-full w-full md:scale-150" />
+        </button>
+
+        <button onclick={() => (value = Math.max(value - 1, min))} class="size-3">
+            <Chevron state={ChevronState.down} class="h-full w-full md:scale-150" />
+        </button>
+    </div>
+</div>
