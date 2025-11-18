d94fa471ee7a03f8cb748778407ff75cc2d1c0e0
Landon Redmond <landon.redmond0@gmail.com>
2025-11-18 11:21:27 -0600

    Refactor increment logic in NumberInput component

--- indxo.app/src/lib/components/NumberInput.svelte
+++ indxo.app/src/lib/components/NumberInput.svelte
@@ -15,24 +15,29 @@
         class?: ClassValue;
         [key: string]: any;
     } = $props();
+
+    function increment(increment: number) {
+        value = Math.min(Math.max(value + increment, min), max);
+    }
 </script>
 
 <div
-    class="input-primary flex-center gap-3"
+    class={["input-primary flex-center gap-3", className]}
     role="spinbutton"
     tabindex="0"
     aria-valuenow={value}
     aria-valuemin={min}
     aria-valuemax={max}
+    {...properties}
 >
     <p>{value}</p>
 
     <div class="flex gap-x-2 md:flex-col">
-        <button onclick={() => (value = Math.min(value + 1, max))} class="size-3">
+        <button onclick={() => increment(1)} class="size-3">
             <Chevron state={ChevronState.up} class="h-full w-full scale-150" />
         </button>
 
-        <button onclick={() => (value = Math.max(value - 1, min))} class="size-3">
+        <button onclick={() => increment(-1)} class="size-3">
             <Chevron state={ChevronState.down} class="h-full w-full scale-150" />
         </button>
     </div>
