f3912042bffde7b1384c65bfee042b025772192b
Landon Redmond <landon.redmond0@gmail.com>
2025-12-08 16:06:06 -0600

    Add ConfirmationPrompt component

---
+++ indxo.app/src/lib/components/ConfirmationPrompt/Button.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+    import { DialogTrigger } from "$lib/components/Dialog";
+    import type { Snippet } from "svelte";
+    import type { ClassValue } from "svelte/elements";
+
+    let {
+        class: className,
+        onClick,
+        children,
+    }: {
+        class?: ClassValue;
+        onClick?: () => void;
+        children: Snippet<[]>;
+    } = $props();
+</script>
+
+<DialogTrigger {onClick} class={className}>
+    {@render children()}
+</DialogTrigger>
---
+++ indxo.app/src/lib/components/ConfirmationPrompt/Content.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+    import type { Snippet } from "svelte";
+    import { DialogContent, DialogContentHeader } from "../Dialog";
+
+    let {
+        title,
+        message,
+        children,
+    }: {
+        title: string;
+        message: string;
+        children: Snippet<[]>;
+    } = $props();
+</script>
+
+<DialogContent>
+    <DialogContentHeader {title} includeCloseButton={false} />
+
+    <p>{message}</p>
+
+    <div class="row mt-5">
+        {@render children()}
+    </div>
+</DialogContent>
