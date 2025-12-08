71eabb064409343005e290fe9e1bc2a4cefcd7b9
Landon Redmond <landon.redmond0@gmail.com>
2025-12-07 22:15:57 -0600

    Smooth EditableListControls container width when scrolling

--- indxo.app/src/lib/components/Lists/Editable/Controls/Controls.svelte
+++ indxo.app/src/lib/components/Lists/Editable/Controls/Controls.svelte
@@ -6,7 +6,7 @@
 
     const viewport: ViewportContext = getContext("viewport");
     let Controls: HTMLDivElement;
-    let distanceToBottom: number = $state.raw(0);
+    let width: number = $state.raw(0);
 
     $effect(() => {
         // Force recalculation on scroll
@@ -18,18 +18,16 @@
             Controls.parentElement.getBoundingClientRect();
         const { bottom: controlsBottom } = Controls.getBoundingClientRect();
         // Subtracting 2 ensures that the controls do hit 100%, without it if there was nothing below the list it would be 99%
-        distanceToBottom = Math.min(
+        const distanceToBottom: number = Math.min(
             100,
-            Math.floor(Math.max(((parentBottom - controlsBottom) / parentHeight) * 100 - 2, 0))
+            Math.max(((parentBottom - controlsBottom) / parentHeight) * 100 - 2, 0)
         );
+        width = distanceToBottom < 25 ? 100 - distanceToBottom : 75;
     });
 </script>
 
 <div class="flex-center sticky bottom-2 z-20 w-full pt-4" {...properties} bind:this={Controls}>
-    <div
-        class="container-primary flex justify-between gap-2 p-2"
-        style:width="{distanceToBottom < 25 ? 100 - distanceToBottom : 75}%"
-    >
+    <div class="container-primary flex justify-between gap-2 p-2" style:width="{width}%">
         {@render children()}
     </div>
 </div>
