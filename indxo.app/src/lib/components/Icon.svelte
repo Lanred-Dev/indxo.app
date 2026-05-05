a4a1f707571708a3cb5e4135776bca4e708e6a18
Landon Redmond <landon.redmond0@gmail.com>
2026-05-05 13:16:41 -0500

    Refactor icons to allow for dynamic coloring

---
+++ indxo.app/src/lib/components/Icon.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    let {
+        icon,
+        class: className,
+    }: {
+        icon: string;
+        class?: string;
+    } = $props();
+</script>
+
+<div class={[className, "[&>svg]:h-full [&>svg]:w-full"]}>
+    {@html icon}
+</div>
