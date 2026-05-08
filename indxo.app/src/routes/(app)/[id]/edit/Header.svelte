f1a75a7aa31326a178a6feaee2b974dc26069f86
Landon Redmond <landon.redmond0@gmail.com>
2026-05-07 22:30:03 -0500

    Replace Chevron component with Icon for improved consistency in Header

--- indxo.app/src/routes/(app)/[id]/edit/Header.svelte
+++ indxo.app/src/routes/(app)/[id]/edit/Header.svelte
@@ -3,16 +3,16 @@
     import determineDocumentType from "$lib/utils/document/determineType";
     import { Wording } from "$lib/utils/wording";
     import { getContext } from "svelte";
-    import Chevron, { ChevronState } from "$lib/components/Icons/Chevron.svelte";
     import { FormSubmit } from "$lib/components/Form";
     import type { DocumentEditContext } from "./+page.svelte";
+    import Icon from "$lib/components/Icon.svelte";
 
     const document: DocumentEditContext = getContext("documentEdit");
 </script>
 
 <div class="mb-5 flex justify-between">
     <a class="button-basic" href="/{document._id}">
-        <Chevron class="size-4" state={ChevronState.left} />
+        <Icon icon="general/Chevrons/Left" />
         Back to {Wording[
             determineDocumentType(document._id) === DocumentType.folder ? "folder" : "set"
         ]}
