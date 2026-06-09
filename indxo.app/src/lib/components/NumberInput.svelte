32a53bd7696731dacd35eb9a3983c0d9a118d8a1
Landon Redmond <landon.redmond0@gmail.com>
2026-06-09 16:27:11 -0500

    Refactor NumberInput component to improve input handling and validation

--- indxo.app/src/lib/components/NumberInput.svelte
+++ indxo.app/src/lib/components/NumberInput.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+    import { onMount } from "svelte";
     import type { ClassValue } from "svelte/elements";
     import Icon from "./Icon.svelte";
 
@@ -16,9 +17,33 @@
         [key: string]: any;
     } = $props();
 
+    let Input: HTMLInputElement;
+    let lastValidValue: number = value;
+
     function increment(increment: number) {
         value = Math.min(Math.max(value + increment, min), max);
     }
+
+    function onInput() {
+        if (Input.value.length === 0) {
+            value = min > 0 ? min : 0;
+            lastValidValue = value;
+            Input.value = "";
+        } else {
+            let newValue: number = parseInt(Input.value.replace(/[^0-9]/g, ""));
+
+            if (!isNaN(newValue)) {
+                lastValidValue = Math.min(Math.max(newValue, min), max);
+                value = lastValidValue;
+            }
+
+            Input.value = lastValidValue.toString();
+        }
+
+        Input.size = Math.max(1, lastValidValue.toString().length);
+    }
+
+    onMount(() => onInput());
 </script>
 
 <div
@@ -30,7 +55,14 @@
     aria-valuemax={max}
     {...properties}
 >
-    <p>{value}</p>
+    <input
+        bind:this={Input}
+        bind:value
+        type="text"
+        inputmode="numeric"
+        class="field-sizing-content w-[2ch] border-none bg-transparent p-0 outline-none focus:ring-0"
+        onblur={onInput}
+    />
 
     <div class="flex gap-x-2 md:flex-col">
         <button onclick={() => increment(1)} class="size-3">
