314c1b612ee4174d60ba8525f46678662243933b
Lanred-Dev <76853738+Lanred-Dev@users.noreply.github.com>
2026-06-14 23:14:46 -0500

    Improve UX and UI on settings page

--- indxo.app/src/routes/(app)/(protected)/settings/+page.svelte
+++ indxo.app/src/routes/(app)/(protected)/settings/+page.svelte
@@ -1,11 +1,5 @@
-<script lang="ts" module>
-    export interface SettingsPageContext {
-        showSubmitMessage: (isError: boolean) => void;
-    }
-</script>
-
 <script lang="ts">
-    import { getContext, setContext, type Component } from "svelte";
+    import { getContext, type Component } from "svelte";
     import Account from "./Account.svelte";
     import SetPreferences from "./SetPreferences.svelte";
     import { SegmentedButton, SegmentedButtonGroup } from "$lib/components/SegmentedButtonGroup";
@@ -13,6 +7,10 @@
     import { Tooltip, TooltipContent } from "$lib/components/Tooltip";
     import type { SessionContext } from "$lib/utils/global";
     import Appearance from "./Appearance.svelte";
+    import FloatingControls from "$lib/components/FloatingControls.svelte";
+    import { slide } from "svelte/transition";
+    import { ResponseCodes } from "$lib/utils/apiResponses";
+    import Loader from "$lib/components/Icons/Loader.svelte";
 
     const pages: {
         [id: string]: {
@@ -38,14 +36,76 @@
         }
     });
     let isSubmitMessageVisible: boolean = $state.raw(false);
-    let isSubmitError: boolean = $state.raw(false);
+    let submitError: string | null = $state.raw(null);
+    let changesMade: boolean = $state.raw(false);
+    let lastSavedName: string = session.user.name;
+    let lastSavedPreferences = JSON.stringify(session.user.preferences);
+    let isSubmitting: boolean = $state.raw(false);
 
-    setContext("settingsPage", {
-        showSubmitMessage(isError: boolean) {
-            isSubmitMessageVisible = true;
-            isSubmitError = isError;
-        },
-    } satisfies SettingsPageContext);
+    function discardChanges() {
+        isSubmitMessageVisible = false;
+        session.user.name = lastSavedName;
+        session.user.preferences = JSON.parse(lastSavedPreferences);
+    }
+
+    async function saveChanges() {
+        isSubmitting = true;
+
+        if (lastSavedName !== session.user.name) {
+            const response = await fetch(`/api/user/${session.user._id}`, {
+                method: "PUT",
+                body: JSON.stringify({ name: session.user.name }),
+            });
+
+            if (response.status !== ResponseCodes.SuccessNoResponse) {
+                try {
+                    submitError = (await response.json()).message;
+                } catch {
+                    submitError = "An unknown error occured when saving your name.";
+                }
+
+                isSubmitMessageVisible = true;
+                return;
+            }
+
+            lastSavedName = session.user.name;
+        }
+
+        if (lastSavedPreferences !== JSON.stringify(session.user.preferences)) {
+            const response = await fetch(`/api/user/${session.user._id}`, {
+                method: "PUT",
+                body: JSON.stringify({
+                    preferences: {
+                        ...session.user.preferences,
+                    },
+                }),
+            });
+
+            if (response.status !== ResponseCodes.SuccessNoResponse) {
+                try {
+                    submitError = (await response.json()).message;
+                } catch {
+                    submitError = "An unknown error occurred when saving your preferences.";
+                }
+
+                isSubmitMessageVisible = true;
+                return;
+            }
+
+            lastSavedPreferences = JSON.stringify(session.user.preferences);
+        }
+
+        submitError = null;
+        isSubmitMessageVisible = true;
+        changesMade = false;
+        isSubmitting = false;
+    }
+
+    $effect(() => {
+        changesMade =
+            session.user.name !== lastSavedName ||
+            JSON.stringify(session.user.preferences) !== lastSavedPreferences;
+    });
 </script>
 
 <svelte:head>
@@ -54,9 +114,9 @@
 </svelte:head>
 
 <Tooltip bind:isVisible={isSubmitMessageVisible} duration={5}>
-    <TooltipContent class={isSubmitError ? "bg-alert" : "bg-success"}>
-        {#if isSubmitError}
-            An error occurred while saving your changes.
+    <TooltipContent class={submitError ? "bg-alert" : "bg-success"}>
+        {#if submitError}
+            {submitError}
         {:else}
             Your changes have been saved successfully.
         {/if}
@@ -84,4 +144,22 @@
     {/each}
 </SegmentedButtonGroup>
 
-<PageComponent />
+<div>
+    <PageComponent />
+</div>
+
+{#if changesMade}
+    <FloatingControls>
+        <button class="button-primary w-full" onclick={discardChanges} disabled={isSubmitting}>
+            Discard Changes
+        </button>
+
+        <button class="button-attention w-full" onclick={saveChanges} disabled={isSubmitting}>
+            {#if isSubmitting}
+                <Loader size={1.75} color="black" />
+            {:else}
+                Save Changes
+            {/if}
+        </button>
+    </FloatingControls>
+{/if}
--- indxo.app/src/routes/(app)/(protected)/settings/SetPreferences.svelte
+++ indxo.app/src/routes/(app)/(protected)/settings/SetPreferences.svelte
@@ -17,25 +17,12 @@
             description="How many times you must get a {Wording.term} wrong that you find difficult before its considered struggling."
         >
             <SectionInput
-                value={session.user.preferences.strugglingTermThreshold}
+                bind:value={session.user.preferences.strugglingTermThreshold}
                 type={SettingInputType.number}
                 placeholder="3"
                 properties={{
                     min: 1,
                     max: 10,
-                }}
-                onUpdate={async (value: number) => {
-                    const response = await fetch(`/api/user/${session.user._id}`, {
-                        method: "PUT",
-                        body: JSON.stringify({
-                            preferences: {
-                                ...session.user.preferences,
-                                strugglingTermThreshold: value,
-                            },
-                        }),
-                    });
-
-                    return response.ok;
                 }}
             />
         </SectionEntry>
@@ -51,22 +38,9 @@
             description="Whether to show the {Wording.term} on the definition side."
         >
             <SectionInput
-                value={session.user.preferences.showTermOnDefinitionSide}
+                bind:value={session.user.preferences.showTermOnDefinitionSide}
                 type={SettingInputType.boolean}
                 placeholder="true"
-                onUpdate={async (value: boolean) => {
-                    const response = await fetch(`/api/user/${session.user._id}`, {
-                        method: "PUT",
-                        body: JSON.stringify({
-                            preferences: {
-                                ...session.user.preferences,
-                                showTermOnDefinitionSide: value,
-                            },
-                        }),
-                    });
-
-                    return response.ok;
-                }}
             />
         </SectionEntry>
 
@@ -75,22 +49,9 @@
             description="Whether to animate {Wording.term} when flipping or cycling through them."
         >
             <SectionInput
-                value={session.user.preferences.animatedTermCards}
+                bind:value={session.user.preferences.animatedTermCards}
                 type={SettingInputType.boolean}
                 placeholder="true"
-                onUpdate={async (value: boolean) => {
-                    const response = await fetch(`/api/user/${session.user._id}`, {
-                        method: "PUT",
-                        body: JSON.stringify({
-                            preferences: {
-                                ...session.user.preferences,
-                                animatedTermCards: value,
-                            },
-                        }),
-                    });
-
-                    return response.ok;
-                }}
             />
         </SectionEntry>
     </SectionContent>
