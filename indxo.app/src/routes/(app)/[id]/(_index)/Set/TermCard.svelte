7b92b58883dc9380e82f34d84e62d3cfd9db4dd8
Lanred-Dev <76853738+Lanred-Dev@users.noreply.github.com>
2026-07-28 19:50:31 -0500

    Refactor TermCard component to use frontImage and backImage props, and improve layout structure

--- indxo.app/src/routes/(app)/[id]/(_index)/Set/TermCard.svelte
+++ indxo.app/src/routes/(app)/[id]/(_index)/Set/TermCard.svelte
@@ -6,7 +6,8 @@
     let {
         term,
         definition,
-        image,
+        frontImage,
+        backImage,
         timesMissed = 0,
         isStrugglingTerm = false,
         index,
@@ -15,26 +16,32 @@
 
 <li
     class={[
-        "container-primary flex gap-3",
+        "container-primary flex flex-col gap-y-1",
         timesMissed > 0 && isStrugglingTerm && "border-alert shadow-alert shadow/40",
         timesMissed > 0 && !isStrugglingTerm && "border-warning shadow-warning shadow/40",
     ]}
 >
-    <p class="text-lg font-semibold">#{index + 1}</p>
+    <div class="flex flex-wrap justify-between gap-x-3 gap-y-1">
+        <p class="text-lg font-semibold">#{index + 1}</p>
 
-    <div class="break-word w-full space-y-2 text-xl">
-        <div class="flex w-full justify-between gap-3">
-            <MarkdownText class="" text={term} />
+        {#if timesMissed > 0}
+            <p class="text-base">You've missed this term {timesMissed} times</p>
+        {/if}
+    </div>
 
-            {#if timesMissed > 0}
-                <p class="text-base">missed {timesMissed} times</p>
-            {/if}
-        </div>
+    <div class="flex gap-x-3 gap-y-1">
+        <MarkdownText text={term} />
 
-        <MarkdownText class="leading-none" text={definition} />
+        {#if frontImage}
+            <ExpandableImage src={frontImage} alt="Front Image" class="h-30 w-auto" />
+        {/if}
+    </div>
 
-        {#if image}
-            <ExpandableImage src={image} alt="Term Image" class="h-30 w-auto" />
+    <div class="flex gap-x-3 gap-y-1">
+        <MarkdownText text={definition} />
+
+        {#if backImage}
+            <ExpandableImage src={backImage} alt="Back Image" class="h-30 w-auto" />
         {/if}
     </div>
 </li>
