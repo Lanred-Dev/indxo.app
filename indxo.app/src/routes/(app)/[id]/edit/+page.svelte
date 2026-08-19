3554074d6287bc75d489e8c7401df0338bc185f3
Landon Redmond <landon.redmond0@gmail.com>
2026-08-19 15:38:38 -0500

    Refactor document context handling in edit page

--- indxo.app/src/routes/(app)/[id]/edit/+page.svelte
+++ indxo.app/src/routes/(app)/[id]/edit/+page.svelte
@@ -8,7 +8,7 @@
 <script lang="ts">
     import { setContext, type Component } from "svelte";
     import Header from "./Header.svelte";
-    import { DocumentType, type DocumentPermission } from "$lib/documents";
+    import { DocumentPermission, DocumentType } from "$lib/documents";
     import { FormContent, FormSubmitMethods } from "$lib/components/Form";
     import Form from "$lib/components/Form/Form.svelte";
     import determineDocumentType from "$lib/utils/document/determineType";
@@ -17,10 +17,29 @@
 
     let { data } = $props();
 
-    setContext("documentEdit", {
-        ...data.document,
-        permission: data.permission,
-    } satisfies DocumentEditContext);
+    let document: DocumentEditContext = $state({ permission: DocumentPermission.none });
+
+    $effect(() => {
+        document = {
+            ...data.document,
+            permission: data.permission,
+        };
+    });
+
+    setContext(
+        "documentEdit",
+        new Proxy({} as DocumentEditContext, {
+            get(_, property: string | symbol) {
+                if (typeof property === "symbol") return undefined;
+                return document[property];
+            },
+            set(_, property: string | symbol, value: unknown) {
+                if (typeof property === "symbol") return true;
+                document[property] = value;
+                return true;
+            },
+        })
+    );
 
     const FormComponent: Component<any> = $derived.by(() => {
         switch (determineDocumentType(data.document._id)) {
